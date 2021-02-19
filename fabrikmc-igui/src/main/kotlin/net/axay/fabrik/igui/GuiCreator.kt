8255cf2e700e4e45267605f9adbab15c4697e5a5
bluefireoly <bluefireolymp@gmail.com>
2021-02-19 20:06:14 +0100

    Update GuiCreator.kt

--- fabrikmc-igui/src/main/kotlin/net/axay/fabrik/igui/GuiCreator.kt
+++ fabrikmc-igui/src/main/kotlin/net/axay/fabrik/igui/GuiCreator.kt
@@ -4,13 +4,10 @@ abstract class GuiCreator {
     abstract fun createInstance(guiData: GuiData): Gui
 }
 
-class SharedGuiCreator : GuiCreator() {
+object SharedGuiCreator : GuiCreator() {
     override fun createInstance(guiData: GuiData) = GuiShared(guiData)
 }
 
-class IndividualGuiCreator(
-    private val resetOnClose: Boolean = true,
-    private val resetOnQuit: Boolean = true
-) : GuiCreator() {
-    override fun createInstance(guiData: GuiData) = GuiIndividual(guiData, resetOnClose, resetOnQuit)
+object IndividualGuiCreator : GuiCreator() {
+    override fun createInstance(guiData: GuiData) = GuiIndividual(guiData)
 }
