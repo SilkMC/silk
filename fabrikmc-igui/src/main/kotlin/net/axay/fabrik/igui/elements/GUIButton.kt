895383f4db7e9a4aae2617ffd30ce1a8183f5a8e
bluefireoly <bluefireolymp@gmail.com>
2021-02-19 19:10:40 +0100

    Update GUIButton.kt

--- fabrikmc-igui/src/main/kotlin/net/axay/fabrik/igui/elements/GUIButton.kt
+++ fabrikmc-igui/src/main/kotlin/net/axay/fabrik/igui/elements/GUIButton.kt
@@ -1,19 +1,18 @@
 package net.axay.fabrik.igui.elements
 
-import net.axay.fabrik.igui.ForInventory
 import net.axay.fabrik.igui.GUIClickEvent
 import net.axay.fabrik.igui.GUIElement
 import net.minecraft.item.ItemStack
 
-open class GUIButton<T : ForInventory>(
+open class GUIButton(
     private val icon: ItemStack,
-    private val action: (GUIClickEvent<T>) -> Unit,
-) : GUIElement<T>() {
+    private val action: (GUIClickEvent) -> Unit,
+) : GUIElement() {
 
     final override fun getItemStack(slot: Int) = icon
 
-    override fun onClickElement(clickEvent: GUIClickEvent<T>) {
-        clickEvent.bukkitEvent.isCancelled = true
+    override fun onClickElement(clickEvent: GUIClickEvent) {
+        clickEvent.isCancelled = true
         action(clickEvent)
     }
 
