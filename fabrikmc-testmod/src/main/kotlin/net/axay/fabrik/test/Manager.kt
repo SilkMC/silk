9d5ac8c7fc31076c316e1fc6a2b9085da6d0a839
bluefireoly <bluefireolymp@gmail.com>
2021-02-22 00:05:06 +0100

    Add commands test

--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/Manager.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/Manager.kt
@@ -1,21 +1,13 @@
 package net.axay.fabrik.test
 
+import net.axay.fabrik.commands.setupRegistrationCallback
 import net.axay.fabrik.core.Fabrik
-import net.axay.fabrik.test.gui.GuiCommand
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.axay.fabrik.test.gui.guiCommand
 
 fun init() {
 
-    ServerLifecycleEvents.SERVER_STARTING.register {
-        println("server starting?")
-    }
-
-    ServerLifecycleEvents.SERVER_STARTED.register {
-        println("server started!?")
-    }
-
     Fabrik.init()
 
-    GuiCommand
+    guiCommand.setupRegistrationCallback()
 
 }
--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/gui/GuiCommand.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/gui/GuiCommand.kt
@@ -1,25 +1,20 @@
 package net.axay.fabrik.test.gui
 
-import com.mojang.brigadier.builder.LiteralArgumentBuilder
-import net.axay.fabrik.igui.*
-import net.axay.fabrik.test.ModCommand
-import net.minecraft.server.command.CommandManager
-import net.minecraft.server.command.ServerCommandSource
+import com.mojang.brigadier.arguments.StringArgumentType
+import net.axay.fabrik.commands.*
+import net.axay.fabrik.igui.openGui
 
-object GuiCommand : ModCommand() {
+private val guis = mapOf(
+    "simpletestgui" to SimpleTestGui.gui
+)
 
-    override val command: LiteralArgumentBuilder<ServerCommandSource> = CommandManager.literal("gui")
-        .then(
-            CommandManager.literal("open").executes {
-
-                try {
-                    it.source.player.openGui(SimpleTestGui.gui)
-                } catch (exc: Throwable) {
-                    exc.printStackTrace()
-                }
-
-                return@executes 1
+val guiCommand = command("gui") {
+    literal("open") {
+        argument("guiname", StringArgumentType.string()) {
+            simpleSuggests { guis.keys }
+            simpleExecutes {
+                it.source.player.openGui(guis[it.getArgument("guiname", String::class.java)]!!)
             }
-        )
-
+        }
+    }
 }
