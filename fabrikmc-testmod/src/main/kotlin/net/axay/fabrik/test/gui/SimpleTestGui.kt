12df15653e41f06afba5ff602ed1a60f2f7ff286
bluefireoly <bluefireolymp@gmail.com>
2021-02-21 14:22:52 +0100

    Create SimpleTestGui.kt

---
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/gui/SimpleTestGui.kt
@@ -0,0 +1,31 @@
+package net.axay.fabrik.test.gui
+
+import net.axay.fabrik.igui.GuiType
+import net.axay.fabrik.igui.PageChangeEffect
+import net.axay.fabrik.igui.Slots
+import net.axay.fabrik.igui.igui
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+
+object SimpleTestGui {
+    val gui get() = igui(GuiType.NINE_BY_THREE) {
+        title = "Moincraft"
+        defaultPage = 1
+
+        page(1) {
+            transitionFrom = PageChangeEffect.SLIDE_HORIZONTALLY
+
+            placeholder(Slots.All, ItemStack(Items.WHITE_STAINED_GLASS_PANE))
+
+            nextPage(Slots.ColumnNine, ItemStack(Items.PAPER))
+        }
+
+        page(2) {
+            transitionFrom = PageChangeEffect.SWIPE_VERTICALLY
+
+            placeholder(Slots.All, ItemStack(Items.BLACK_STAINED_GLASS_PANE))
+
+            previousPage(Slots.ColumnOne, ItemStack(Items.PAPER))
+        }
+    }
+}
