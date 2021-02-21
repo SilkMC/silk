2b672a87a14600dffeef1bf5d577c84cfeef6100
bluefireoly <bluefireolymp@gmail.com>
2021-02-22 00:08:53 +0100

    Add error handling to gui test command

--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/Manager.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/Manager.kt
@@ -2,7 +2,7 @@ package net.axay.fabrik.test
 
 import net.axay.fabrik.commands.setupRegistrationCallback
 import net.axay.fabrik.core.Fabrik
-import net.axay.fabrik.test.gui.guiCommand
+import net.axay.fabrik.test.commands.guiCommand
 
 fun init() {
 
--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/GuiCommand.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/GuiCommand.kt
@@ -1,8 +1,10 @@
-package net.axay.fabrik.test.gui
+package net.axay.fabrik.test.commands
 
 import com.mojang.brigadier.arguments.StringArgumentType
 import net.axay.fabrik.commands.*
 import net.axay.fabrik.igui.openGui
+import net.axay.fabrik.test.gui.SimpleTestGui
+import net.minecraft.text.LiteralText
 
 private val guis = mapOf(
     "simpletestgui" to SimpleTestGui.gui
@@ -13,7 +15,10 @@ val guiCommand = command("gui") {
         argument("guiname", StringArgumentType.string()) {
             simpleSuggests { guis.keys }
             simpleExecutes {
-                it.source.player.openGui(guis[it.getArgument("guiname", String::class.java)]!!)
+                val gui = guis[it.getArgument("guiname", String::class.java)]
+                if (gui != null)
+                    it.source.player.openGui(gui)
+                else it.source.sendError(LiteralText("Dieses GUI existiert nicht"))
             }
         }
     }
