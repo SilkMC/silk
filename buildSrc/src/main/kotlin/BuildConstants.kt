8c09efc5eeb39432db376361d383e84223f0e67a
bluefireoly <bluefireolymp@gmail.com>
2021-02-25 18:58:22 +0100

    Update BuildConstants.kt

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -1,4 +1,15 @@
 object BuildConstants {
-    const val minecraftVersion = "1.16.5"
     const val projectState = "alpha"
+
+    const val curseforgeId = "447425"
+    const val githubRepo = "bluefireoly/fabrikmc"
+
+    // check these values here:
+    // - https://modmuss50.me/fabric.html
+    // - https://www.curseforge.com/minecraft/mc-mods/fabric-language-kotlin/files
+    const val minecraftVersion = "1.16.5"
+    const val fabricApiVersion = "0.31.0+1.16"
+    const val yarnMappingsVersion = "1.16.5+build.5:v2"
+    const val loaderVersion = "0.11.2"
+    const val fabricLanguageKotlinVersion = "1.4.30+build.2"
 }
