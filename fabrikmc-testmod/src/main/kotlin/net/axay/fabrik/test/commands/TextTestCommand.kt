cc13eea18d8a6bf43789abf713d48520b748b7b7
bluefireoly <bluefireolymp@gmail.com>
2021-02-28 02:47:31 +0100

    Create TextTestCommand.kt

---
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/TextTestCommand.kt
@@ -0,0 +1,46 @@
+package net.axay.fabrik.test.commands
+
+import com.mojang.brigadier.arguments.StringArgumentType.string
+import net.axay.fabrik.commands.*
+import net.axay.fabrik.core.text.literalText
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.HoverEvent
+
+private val textExamples = mapOf(
+    "welcome" to literalText {
+        text {
+            color = 0xFF5E13
+            text("Welcome on ")
+            text("our") {
+                underline = true
+            }
+            text(" Server") {
+                color = 0x21FF9E
+            }
+            text("!")
+        }
+        text(" ")
+        text {
+            color = 0xFDFF80
+            hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, literalText("nice, you did it") {
+                bold = true
+                color = 0x39CEFF
+            })
+            text("[")
+            text("hover and click here") {
+                color = 0xFFE12C
+                clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/me hey :)")
+            }
+            text("]")
+        }
+    }
+)
+
+val textTestCommand = command("text") {
+    argument("textexample", string()) {
+        simpleSuggests { textExamples.keys }
+        simpleExecutes {
+            it.source.sendFeedback(textExamples[it.getArgument("textexample")], false)
+        }
+    }
+}
