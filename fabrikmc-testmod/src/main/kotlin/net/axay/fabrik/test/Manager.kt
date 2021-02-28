5b9d7d7c9a1a1648d90b4b977a1dce54dd604516
bluefireoly <bluefireolymp@gmail.com>
2021-02-28 02:47:42 +0100

    Register textTestCommand

--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/Manager.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/Manager.kt
@@ -3,11 +3,13 @@ package net.axay.fabrik.test
 import net.axay.fabrik.commands.setupRegistrationCallback
 import net.axay.fabrik.core.Fabrik
 import net.axay.fabrik.test.commands.guiCommand
+import net.axay.fabrik.test.commands.textTestCommand
 
 fun init() {
 
     Fabrik.init()
 
     guiCommand.setupRegistrationCallback()
+    textTestCommand.setupRegistrationCallback()
 
 }
