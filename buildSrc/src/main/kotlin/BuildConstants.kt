4044c48f53452339b566913adca153fc0d312d48
Jakob K <bluefireolymp@gmail.com>
2021-03-15 17:54:11 +0100

    Update BuildConstants.kt

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -9,8 +9,8 @@ object BuildConstants {
     // - https://modmuss50.me/fabric.html
     // - https://www.curseforge.com/minecraft/mc-mods/fabric-language-kotlin/files
     const val minecraftVersion = "1.16.5"
-    const val fabricApiVersion = "0.31.0+1.16"
+    const val fabricApiVersion = "0.32.0+1.16"
     const val yarnMappingsVersion = "1.16.5+build.5:v2"
-    const val loaderVersion = "0.11.2"
+    const val loaderVersion = "0.11.3"
     const val fabricLanguageKotlinVersion = "1.4.30+build.2"
 }
