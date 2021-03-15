ede8e67ce7e758b7ea915e5de5a0ab1d61630923
Jakob K <bluefireolymp@gmail.com>
2021-03-15 18:53:12 +0100

    Create IdentifierSerializer.kt

---
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/serialization/serializers/IdentifierSerializer.kt
@@ -0,0 +1,17 @@
+package net.axay.fabrik.core.serialization.serializers
+
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import net.axay.fabrik.core.serialization.FabrikSerializer
+import net.minecraft.util.Identifier
+
+class IdentifierSerializer : FabrikSerializer<Identifier>() {
+    override fun deserialize(decoder: Decoder): Identifier {
+        val split = decoder.decodeString().split(':')
+        return Identifier(split[0], split[1])
+    }
+
+    override fun serialize(encoder: Encoder, value: Identifier) {
+        encoder.encodeString("${value.namespace}:${value.path}")
+    }
+}
