0b8052296e7f8777c696f4ccd23327f784efb98d
Jakob K <bluefireolymp@gmail.com>
2021-03-15 17:54:04 +0100

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.4.30"))
+    implementation(kotlin("gradle-plugin", "1.4.31"))
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.4.31")
     implementation("net.fabricmc", "fabric-loom", "0.6-SNAPSHOT")
     implementation("gradle.plugin.com.matthewprenger:CurseGradle:1.4.0")
 }
