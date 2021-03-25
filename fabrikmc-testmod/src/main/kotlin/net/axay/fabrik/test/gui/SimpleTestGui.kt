3cfdc521c5fcf548f8bcb39ea6520c3f91ad2c6c
Jakob K <bluefireolymp@gmail.com>
2021-03-25 16:50:19 +0100

    Update SimpleTestGui.kt

--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/gui/SimpleTestGui.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/gui/SimpleTestGui.kt
@@ -1,31 +1,24 @@
 package net.axay.fabrik.test.gui
 
-import net.axay.fabrik.igui.GuiType
-import net.axay.fabrik.igui.PageChangeEffect
-import net.axay.fabrik.igui.Slots
-import net.axay.fabrik.igui.igui
+import net.axay.fabrik.igui.*
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 
 object SimpleTestGui {
-    val gui get() = igui(GuiType.NINE_BY_THREE) {
-        title = "Moincraft"
-        defaultPage = 1
-
+    val gui get() = igui(GuiType.NINE_BY_THREE, "Moincraft", "1") {
         page(1) {
-            transitionFrom = PageChangeEffect.SLIDE_HORIZONTALLY
-            transitionTo = PageChangeEffect.SLIDE_HORIZONTALLY
+            effectFrom = GuiPage.ChangeEffect.SLIDE_HORIZONTALLY
+            effectTo = GuiPage.ChangeEffect.SLIDE_HORIZONTALLY
 
             placeholder(Slots.All, ItemStack(Items.WHITE_STAINED_GLASS_PANE))
 
-            nextPage(Slots.ColumnNine, ItemStack(Items.PAPER))
+            nextPage(2 sl 9, ItemStack(Items.PAPER))
         }
 
         page(2) {
-
             placeholder(Slots.All, ItemStack(Items.BLACK_STAINED_GLASS_PANE))
 
-            previousPage(Slots.ColumnOne, ItemStack(Items.PAPER))
+            previousPage(2 sl 1, ItemStack(Items.PAPER))
         }
     }
 }
