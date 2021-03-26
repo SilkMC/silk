54c3bd7c83052c52544b92efa4570fca9e37cb83
Jakob K <bluefireolymp@gmail.com>
2021-03-26 19:46:19 +0100

    Update TextExtensions.kt

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/text/TextExtensions.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/text/TextExtensions.kt
@@ -2,6 +2,7 @@ package net.axay.fabrik.core.text
 
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.text.LiteralText
+import net.minecraft.text.Text
 
 /**
  * Sends the given [LiteralText] to the player.
@@ -14,3 +15,10 @@ fun PlayerEntity.sendText(text: LiteralText) {
  * Converts this string to a [LiteralText] instance.
  */
 val String.literal get() = LiteralText(this)
+
+/**
+ * Returns a list of [Text] elements which all do not exceed
+ * the given width.
+ */
+inline fun String.literalLines(width: Int, lineBuilder: (line: String) -> Text) =
+    chunked(width).map { lineBuilder.invoke(it) }
