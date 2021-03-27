9bca7a338366cab25195a6384ba82a478e8ff118
Jakob K <bluefireolymp@gmail.com>
2021-03-27 13:30:37 +0100

    Improve the literalLines function

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/text/TextExtensions.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/text/TextExtensions.kt
@@ -3,6 +3,7 @@ package net.axay.fabrik.core.text
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.text.LiteralText
 import net.minecraft.text.Text
+import org.apache.commons.lang3.text.WordUtils
 
 /**
  * Sends the given [LiteralText] to the player.
@@ -19,6 +20,15 @@ val String.literal get() = LiteralText(this)
 /**
  * Returns a list of [Text] elements which all do not exceed
  * the given width.
+ *
+ * @param width the maximum width of one line
+ * @param cutLongWords if true, the maximum width will always be enforced
+ * but cutting long words in the middle
+ * @param lineBuilder responsible for building the literal of each line
  */
-inline fun String.literalLines(width: Int = 40, lineBuilder: (line: String) -> Text = { it.literal }) =
-    chunked(width).map { lineBuilder.invoke(it) }
+inline fun String.literalLines(
+    width: Int = 40,
+    cutLongWords: Boolean = true,
+    lineBuilder: (line: String) -> Text = { it.literal }
+): List<Text> = WordUtils.wrap(this, width, System.lineSeparator(), cutLongWords).split(System.lineSeparator())
+    .map(lineBuilder)
