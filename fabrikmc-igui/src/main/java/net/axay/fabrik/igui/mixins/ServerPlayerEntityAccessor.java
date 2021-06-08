f83a6a6f6e71bd6a46f933be0ee12c9fb4182aa9
Jakob K <bluefireolymp@gmail.com>
2021-06-08 16:22:56 +0200

    Create ServerPlayerEntityAccessor.java

---
+++ fabrikmc-igui/src/main/java/net/axay/fabrik/igui/mixins/ServerPlayerEntityAccessor.java
@@ -0,0 +1,12 @@
+package net.axay.fabrik.igui.mixins;
+
+import net.minecraft.screen.ScreenHandlerSyncHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ServerPlayerEntity.class)
+public interface ServerPlayerEntityAccessor {
+    @Accessor("screenHandlerSyncHandler")
+    ScreenHandlerSyncHandler getScreenHandlerSyncHandler();
+}
