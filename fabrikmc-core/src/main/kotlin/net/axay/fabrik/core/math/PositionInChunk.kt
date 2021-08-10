e02809030dc25676163a526fccb3b0a752ef8697
Jakob K <bluefireolymp@gmail.com>
2021-08-10 17:19:36 +0200

    Update PositionInChunk.kt

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/math/PositionInChunk.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/math/PositionInChunk.kt
@@ -3,9 +3,7 @@ package net.axay.fabrik.core.math
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.ChunkPos
 
-data class PositionInChunk(
-    val x: Int, val y: Int, val z: Int
-) {
+data class PositionInChunk(val x: Int, val y: Int, val z: Int) {
     constructor(blockPos: BlockPos) : this(blockPos.x and 15, blockPos.y, blockPos.z and 15)
 
     fun getBlockPos(chunkPos: ChunkPos) = BlockPos(chunkPos.startX + x, y, chunkPos.startZ + z)
