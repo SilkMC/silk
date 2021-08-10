256f445137b66bc722a5833425173d6377148aa6
Jakob K <bluefireolymp@gmail.com>
2021-08-10 19:31:05 +0200

    Use the new sideboard scoreboard

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/scoreboard/sideboard/Sideboard.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/scoreboard/sideboard/Sideboard.kt
@@ -2,11 +2,8 @@ package net.axay.fabrik.core.scoreboard.sideboard
 
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import net.axay.fabrik.core.Fabrik
+import net.axay.fabrik.core.scoreboard.FabrikSideboardScoreboard
 import net.axay.fabrik.core.task.fabrikCoroutineScope
-import net.minecraft.scoreboard.Scoreboard
-import net.minecraft.scoreboard.ScoreboardCriterion
-import net.minecraft.scoreboard.ServerScoreboard
 import net.minecraft.text.Text
 
 class Sideboard(
@@ -14,20 +11,13 @@ class Sideboard(
     displayName: Text,
     lines: List<SideboardLine>,
 ) {
-    companion object {
-        val sidebarId = Scoreboard.getDisplaySlotId("sidebar")
-    }
-
-    private val scoreboard = ServerScoreboard(Fabrik.currentServer)
-    private val objective = scoreboard.addObjective(name, ScoreboardCriterion.DUMMY, displayName, ScoreboardCriterion.RenderType.INTEGER)
+    private val scoreboard = FabrikSideboardScoreboard(name, displayName)
 
     init {
-        scoreboard.setObjectiveSlot(sidebarId, objective)
-
         lines.forEachIndexed { index, line ->
             val team = scoreboard.addTeam("team_$index")
             scoreboard.addPlayerToTeam("§$index", team)
-            scoreboard.getPlayerScore("§$index", objective).score = lines.size - index
+            scoreboard.setPlayerScore("§$index", lines.size - index)
 
             fabrikCoroutineScope.launch {
                 line.textFlow.collect {
