b4414d506aa4977c50ec0a1de8659ab987d6202c
Jakob K <bluefireolymp@gmail.com>
2021-08-10 19:33:31 +0200

    Use flowOf for single value flow

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/scoreboard/sideboard/SideboardLine.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/scoreboard/sideboard/SideboardLine.kt
@@ -1,7 +1,7 @@
 package net.axay.fabrik.core.scoreboard.sideboard
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import net.minecraft.text.Text
 
 interface SideboardLine {
@@ -9,7 +9,7 @@ interface SideboardLine {
 }
 
 class SimpleSideboardLine(text: Text) : SideboardLine {
-    override val textFlow = flow { emit(text) }
+    override val textFlow = flowOf(text)
 }
 
 class ChangingSideboardLine(override val textFlow: Flow<Text>) : SideboardLine
