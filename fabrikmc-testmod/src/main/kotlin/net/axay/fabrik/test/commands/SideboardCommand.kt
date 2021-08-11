41d972d425c4da9400d4d7da6ccd7b93d546e122
Jakob K <bluefireolymp@gmail.com>
2021-08-11 19:18:10 +0200

    Use new sideboard builder in example mod

--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/SideboardCommand.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/SideboardCommand.kt
@@ -2,34 +2,31 @@ package net.axay.fabrik.test.commands
 
 import com.mojang.brigadier.arguments.StringArgumentType
 import net.axay.fabrik.commands.*
-import net.axay.fabrik.core.scoreboard.Sideboard
-import net.axay.fabrik.core.text.literal
+import net.axay.fabrik.core.scoreboard.sideboard.showSideboard
+import net.axay.fabrik.core.scoreboard.sideboard.sideboard
 import net.axay.fabrik.core.text.literalText
 
 val sideboardCommand = command("sideboard", true) {
     argument("example", StringArgumentType.string()) {
         simpleSuggests { sideboardExamples.keys }
         simpleExecutes {
-            sideboardExamples[it.getArgument("example")]?.value
+            val sideboard = sideboardExamples[it.getArgument("example")] ?: return@simpleExecutes
+            it.source.player.showSideboard(sideboard)
         }
     }
 }
 
 private val sideboardExamples = mapOf(
-    "simple" to lazy {
-        Sideboard(
-            "simple",
-            literalText("Simple Sideboard") {
-                color = 0x6DFF41
-            },
-            listOf(
-                "Hey wie,".literal,
-                "geht's?".literal,
-                " ".literal,
-                literalText("Farben gehen auch") {
-                    color = 0x57FFF0
-                }
-            )
-        )
+    "simple" to sideboard(
+        literalText("Simple Sideboard") { color = 0x6DFF41 }
+    ) {
+        literalLine("Hey wie,")
+        literalLine("geht's?")
+        literalLine(" ")
+        lineChangingPeriodically(1000) {
+            literalText("Farben gehen auch") {
+                color = (0x000000..0xFFFFFF).random()
+            }
+        }
     }
 )
