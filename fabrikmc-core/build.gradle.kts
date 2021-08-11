b61b964a46b0d9d41afffae3c9328dc5411752c6
Jakob K <bluefireolymp@gmail.com>
2021-08-11 20:43:57 +0200

    Update serialization plugin

--- fabrikmc-core/build.gradle.kts
+++ fabrikmc-core/build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     `java-version-script`
     `mod-build-script`
     `mod-publish-script`
-    kotlin("plugin.serialization") version "1.5.10"
+    kotlin("plugin.serialization") version "1.5.21"
 }
