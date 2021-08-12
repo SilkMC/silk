aebf44b3abe5a028a69de4f52c9102649b1608f1
Jakob K <bluefireolymp@gmail.com>
2021-08-12 19:27:48 +0200

    Add documentation to persistence module

--- fabrikmc-persistence/src/main/kotlin/net/axay/fabrik/persistence/AccessExtensions.kt
+++ fabrikmc-persistence/src/main/kotlin/net/axay/fabrik/persistence/AccessExtensions.kt
@@ -2,4 +2,9 @@ package net.axay.fabrik.persistence
 
 import net.minecraft.world.chunk.Chunk
 
+/**
+ * Returns a persistent [net.minecraft.nbt.NbtCompound].
+ *
+ * @see CompoundProvider
+ */
 val Chunk.persistentCompound get() = (this as CompoundProvider).compound
--- fabrikmc-persistence/src/main/kotlin/net/axay/fabrik/persistence/CompoundProvider.kt
+++ fabrikmc-persistence/src/main/kotlin/net/axay/fabrik/persistence/CompoundProvider.kt
@@ -2,6 +2,12 @@ package net.axay.fabrik.persistence
 
 import net.minecraft.nbt.NbtCompound
 
+/**
+ * This class provides an [NbtCompound] which is persistent.
+ *
+ * Everything you write to this compound will be saved to disk, and can
+ * be accessed at any time later.
+ */
 interface CompoundProvider {
     var compound: NbtCompound
 }
