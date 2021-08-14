2a375262c8b557e34dfc93e2492363442d573be1
F0X <f0xde@protonmail.com>
2021-08-14 12:07:14 +0200

    Expand tests

--- fabrikmc-nbt/src/test/kotlin/net/axay/fabrik/nbt/NbtEncodingTest.kt
+++ fabrikmc-nbt/src/test/kotlin/net/axay/fabrik/nbt/NbtEncodingTest.kt
@@ -7,20 +7,30 @@ import io.kotest.property.Arb
 import io.kotest.property.arbitrary.*
 import io.kotest.property.checkAll
 import kotlinx.serialization.Serializable
-import net.minecraft.nbt.NbtByteArray
-import net.minecraft.nbt.NbtCompound
-import net.minecraft.nbt.NbtIntArray
-import net.minecraft.nbt.NbtLongArray
+import net.minecraft.nbt.*
 
-class NbtTest : StringSpec({
+class NbtEncodingTest : StringSpec({
     "class should encode to compound" {
-        val value = TestClass(10, -1248, "Taito")
+        val value = TestClass(
+            10,
+            -1248,
+            "Taito",
+            listOf("abc", "äèðßèäßð", "1 2"),
+            setOf(5, 34, 9, 3443),
+            InnerTestClass(true)
+        )
         val element = Nbt.encodeToNbtElement(value)
         element.shouldBeInstanceOf<NbtCompound>()
-        element.size shouldBe 3
+        element.size shouldBe 6
         element.getInt("x") shouldBe value.x
         element.getLong("y") shouldBe value.y
         element.getString("name") shouldBe value.name
+        element.getList("stringList", NbtElement.STRING_TYPE.toInt()).map { it.asString() } shouldBe value.stringList
+        element.getLongArray("longSet") shouldBe value.longSet.toLongArray()
+        with(element.getCompound("inner")) {
+            size shouldBe 1
+            getBoolean("test") shouldBe value.inner.test
+        }
     }
 
     "byte collections should encode to byte array" {
@@ -41,7 +51,7 @@ class NbtTest : StringSpec({
         }
     }
 
-    "int collections should encode to byte array" {
+    "int collections should encode to int array" {
         checkAll(Arb.list(Arb.int(), 0..0x1000)) {
             val array = it.toIntArray()
             val elements = listOf(Nbt.encodeToNbtElement(it), Nbt.encodeToNbtElement(array))
@@ -57,7 +67,7 @@ class NbtTest : StringSpec({
         }
     }
 
-    "long collections should encode to byte array" {
+    "long collections should encode to long array" {
         checkAll(Arb.list(Arb.long(), 0..0x1000)) {
             val array = it.toLongArray()
             val elements = listOf(Nbt.encodeToNbtElement(it), Nbt.encodeToNbtElement(array))
@@ -75,4 +85,14 @@ class NbtTest : StringSpec({
 })
 
 @Serializable
-class TestClass(val x: Int, val y: Long, val name: String)
+class TestClass(
+    val x: Int,
+    val y: Long,
+    val name: String,
+    val stringList: List<String>,
+    val longSet: Set<Long>,
+    val inner: InnerTestClass,
+)
+
+@Serializable
+class InnerTestClass(val test: Boolean)
