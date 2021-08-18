f74283f50ed1a02242f2654b92518dafe2f19fe4
Jakob K <bluefireolymp@gmail.com>
2021-08-18 20:21:19 +0200

    Change access to context in SimpleCommandContext

--- fabrikmc-commands/src/main/kotlin/net/axay/fabrik/commands/SimpleCommandContext.kt
+++ fabrikmc-commands/src/main/kotlin/net/axay/fabrik/commands/SimpleCommandContext.kt
@@ -4,21 +4,39 @@ import com.mojang.brigadier.RedirectModifier
 import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.context.ParsedArgument
-import net.axay.fabrik.commands.mixin.CommandContextAccessor
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.isAccessible
+
+// the following class and all usages of reflection in it are only required until kotlin gets
+// the context keyword for extension functions
 
 @Suppress("UNCHECKED_CAST")
 class SimpleCommandContext<S>(context: CommandContext<S>) : CommandContext<S>(
     context.source,
     context.input,
-    (context as CommandContextAccessor).arguments as MutableMap<String, ParsedArgument<S, *>>,
+    commandContextArgumentsField.get(context) as MutableMap<String, ParsedArgument<S, *>>?,
     context.command,
     context.rootNode,
     context.nodes,
     context.range,
     context.child,
-    context.modifier as RedirectModifier<S>,
-    context.forks
+    commandContextModifierField.get(context) as RedirectModifier<S>?,
+    commandContextForksField.get(context) as Boolean
 ) {
+    companion object {
+        private val commandContextArgumentsField = CommandContext::class.declaredMemberProperties
+            .first { it.name == "arguments" }
+            .apply { isAccessible = true }
+
+        private val commandContextModifierField = CommandContext::class.declaredMemberProperties
+            .first { it.name == "modifier" }
+            .apply { isAccessible = true }
+
+        private val commandContextForksField = CommandContext::class.declaredMemberProperties
+            .first { it.name == "forks" }
+            .apply { isAccessible = true }
+    }
+
     /**
      * Resolves the argument specified by this context builder function of the
      * type [RequiredArgumentBuilder].
