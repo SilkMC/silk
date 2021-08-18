6df116d32a247bce5208f14fcc90498ba737218a
Jakob K <bluefireolymp@gmail.com>
2021-08-18 18:14:45 +0200

    Split the sideboard demos

--- fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/SideboardCommand.kt
+++ fabrikmc-testmod/src/main/kotlin/net/axay/fabrik/test/commands/SideboardCommand.kt
@@ -22,11 +22,22 @@ private val sideboardExamples = mapOf(
     "simple" to sideboard(
         literalText("Simple Sideboard") { color = 0x6DFF41 }
     ) {
-        literalLine("Hey wie,")
-        literalLine("geht's?")
+        literalLine("Hey, how")
+        literalLine("are you?")
+        literalLine(" ")
+        literalLine("colors work as well!") {
+            color = 0xFF9658
+        }
+    },
+
+    "simple_changing" to sideboard(
+        literalText("Simple Sideboard") { color = 0x6DFF41 }
+    ) {
+        literalLine("Hey, how")
+        literalLine("are you?")
         literalLine(" ")
         lineChangingPeriodically(1000) {
-            literalText("Farben gehen auch") {
+            literalText("changing color") {
                 color = (0x000000..0xFFFFFF).random()
             }
         }
