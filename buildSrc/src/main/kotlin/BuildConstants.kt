509f32f7ed99aa24383ddca1ce0b11ec67803b17
Jakob K <bluefireolymp@gmail.com>
2021-08-20 02:27:11 +0200

    Update BuildConstants.kt

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -15,9 +15,9 @@ object BuildConstants {
 
     // check these values here: https://axay.net/fabric/gradlekts/latest
     const val minecraftVersion = "1.17.1"
-    const val yarnMappingsVersion = "1.17.1+build.31:v2"
+    const val yarnMappingsVersion = "1.17.1+build.39:v2"
     const val fabricLoaderVersion = "0.11.6"
-    const val fabricApiVersion = "0.37.1+1.17"
+    const val fabricApiVersion = "0.38.0+1.17"
     const val fabricLanguageKotlinVersion = "1.6.3+kotlin.1.5.21"
 
     const val kotestVersion = "4.6.1"
