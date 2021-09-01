1d86cefa368a4ddf90cb69430c1a96f5b86bba4a
Jakob K <bluefireolymp@gmail.com>
2021-09-01 20:57:36 +0200

    Increase development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.versioning.VersioningConfiguration
 import org.jetbrains.dokka.versioning.VersioningPlugin
 
-version = "1.2.2"
+version = "1.3.0"
 
 plugins {
     id("org.jetbrains.dokka")
