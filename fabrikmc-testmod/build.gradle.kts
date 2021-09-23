3cf6ea46f830e221944574d4f783bcf1ec21706e
Jakob K <bluefireolymp@gmail.com>
2021-09-23 21:16:48 +0200

    Add game module to testmod

--- fabrikmc-testmod/build.gradle.kts
+++ fabrikmc-testmod/build.gradle.kts
@@ -7,9 +7,10 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":${rootProject.name}-core"))
-    implementation(project(":${rootProject.name}-igui"))
     implementation(project(":${rootProject.name}-commands"))
+    implementation(project(":${rootProject.name}-core"))
+    implementation(project(":${rootProject.name}-game"))
+    implementation(project(":${rootProject.name}-igui"))
     implementation(project(":${rootProject.name}-persistence"))
 }
 
