b597490d067dd1e8d5564ed7dea02c6da207474f
Jakob K <bluefireolymp@gmail.com>
2021-09-23 17:50:12 +0200

    Increase versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.versioning.VersioningConfiguration
 import org.jetbrains.dokka.versioning.VersioningPlugin
 
-version = "1.4.4"
+version = "1.4.5"
 
 plugins {
     id("org.jetbrains.dokka")
--- shared.properties.gradle.kts
+++ shared.properties.gradle.kts
@@ -1,2 +1,2 @@
-val kotlinVersion by extra("1.5.30")
+val kotlinVersion by extra("1.5.31")
 val dokkaVersion by extra("1.5.30")
