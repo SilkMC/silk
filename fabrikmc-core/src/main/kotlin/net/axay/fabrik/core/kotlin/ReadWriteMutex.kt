3baa5770ad6701daf3d1b87dc07a4170a9299261
Jakob K <bluefireolymp@gmail.com>
2021-10-09 19:41:00 +0200

    Create ReadWriteMutex.kt

---
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/kotlin/ReadWriteMutex.kt
@@ -0,0 +1,52 @@
+package net.axay.fabrik.core.kotlin
+
+import kotlinx.coroutines.sync.Mutex
+
+class ReadWriteMutex {
+    private var readers = 0
+
+    private val readCheckMutex = Mutex()
+    private val writeMutex = Mutex()
+
+    suspend fun beginRead() {
+        readCheckMutex.lock()
+        readers++
+        if (readers == 1)
+            writeMutex.lock()
+        readCheckMutex.unlock()
+    }
+
+    suspend fun endRead() {
+        readCheckMutex.lock()
+        readers--
+        if (readers == 0)
+            writeMutex.unlock()
+        readCheckMutex.unlock()
+    }
+
+    suspend fun beginWrite() {
+        writeMutex.lock()
+    }
+
+    fun endWrite() {
+        writeMutex.unlock()
+    }
+
+    suspend inline fun <T> read(block: () -> T): T {
+        beginRead()
+        try {
+            return block()
+        } finally {
+            endRead()
+        }
+    }
+
+    suspend inline fun <T> write(block: () -> T): T {
+        beginWrite()
+        try {
+            return block()
+        } finally {
+            endWrite()
+        }
+    }
+}
