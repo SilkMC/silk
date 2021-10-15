74ca07bd760803d6f393d3387bd35e27e3144e1b
Jakob K <bluefireolymp@gmail.com>
2021-10-15 22:21:44 +0200

    Fix network module buildscript

--- fabrikmc-network/build.gradle.kts
+++ fabrikmc-network/build.gradle.kts
@@ -9,8 +9,12 @@ plugins {
 }
 
 val modName by extra("$projectTitle Network")
+val modEntrypoints by extra(linkedMapOf(
+    "main" to listOf("net.axay.fabrik.network.internal.FabrikNetwork"),
+    "client" to listOf("net.axay.fabrik.network.internal.FabrikNetwork"),
+))
 
 dependencies {
     implementation(project(":${rootProject.name}-core"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.0")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.0")
 }
