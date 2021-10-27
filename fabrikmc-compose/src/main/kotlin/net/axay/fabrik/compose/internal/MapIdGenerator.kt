3e06f6536ec95470236f28ca89445d5f2b5af381
Jakob K <bluefireolymp@gmail.com>
2021-10-28 00:24:37 +0200

    Use Mutex in MapIdGenerator

--- fabrikmc-compose/src/main/kotlin/net/axay/fabrik/compose/internal/MapIdGenerator.kt
+++ fabrikmc-compose/src/main/kotlin/net/axay/fabrik/compose/internal/MapIdGenerator.kt
@@ -1,15 +1,29 @@
 package net.axay.fabrik.compose.internal
 
-import java.util.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import net.axay.fabrik.core.task.fabrikCoroutineScope
 
 object MapIdGenerator {
+    val mutex = Mutex()
+
     private var currentMaxId = Int.MAX_VALUE - 12000
-        @Synchronized get
-        @Synchronized set
 
-    private val availableOldIds = Vector<Int>()
+    private val availableOldIds = ArrayList<Int>()
 
-    fun nextId() = availableOldIds.removeFirstOrNull() ?: ++currentMaxId
+    fun nextId() = runBlocking {
+        mutex.withLock {
+            availableOldIds.removeFirstOrNull() ?: (++currentMaxId)
+        }
+    }
 
-    fun makeOldIdAvailable(id: Collection<Int>) = availableOldIds.addAll(id)
+    fun makeOldIdAvailable(ids: Collection<Int>) {
+        fabrikCoroutineScope.launch {
+            mutex.withLock {
+                availableOldIds.addAll(ids)
+            }
+        }
+    }
 }
