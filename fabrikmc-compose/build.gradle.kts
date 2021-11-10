91b0cb7bc29ce650a28f190d6a45846e776cf60b
Jakob K <bluefireolymp@gmail.com>
2021-11-10 20:19:13 +0100

    Update multik

--- fabrikmc-compose/build.gradle.kts
+++ fabrikmc-compose/build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
     implementation(project(":${rootProject.name}-core"))
     implementation(project(":${rootProject.name}-commands"))
 
-    api("org.jetbrains.kotlinx:multik-api:0.1.0")
-    api("org.jetbrains.kotlinx:multik-default:0.1.0")
+    api("org.jetbrains.kotlinx:multik-api:0.1.1")
+    api("org.jetbrains.kotlinx:multik-default:0.1.1")
 
     api("com.github.ajalt.colormath:colormath:3.1.1")
 }
