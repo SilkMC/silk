990b45b28f2534a10fce9239c97d5d5f95c2791d
Jakob K <bluefireolymp@gmail.com>
2021-11-17 23:56:14 +0100

    Remove unused group and version declaration

--- fabrikmc-compose/fabrikmc-compose-mojang-api/build.gradle.kts
+++ fabrikmc-compose/fabrikmc-compose-mojang-api/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
     kotlin("plugin.serialization")
 }
 
-group = "net.axay"
-version = rootProject.version
-
 description = "FabrikMC Compose Mojang API extracts Minecraft assets from the client jar"
 
 repositories {
