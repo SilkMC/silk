b850bb546b237e92627e649d8f69b3c14d151da3
Jakob K <dev@jakobk.de>
2021-11-23 13:22:23 +0100

    Update Dokka docs

--- docs/older/1.5.2/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":fabrikmc-nbt:dokkaHtmlPartial/main":[],":fabrikmc-network:dokkaHtmlPartial/main":[],":fabrikmc-game:dokkaHtmlPartial/main":[],":fabrikmc-commands:dokkaHtmlPartial/main":[],":fabrikmc-igui:dokkaHtmlPartial/main":[],":fabrikmc-core:dokkaHtmlPartial/main":[],":fabrikmc-persistence:dokkaHtmlPartial/main":[]}'
