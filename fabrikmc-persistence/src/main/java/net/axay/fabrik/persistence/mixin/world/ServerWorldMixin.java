c0fc8e730c846b406614fb9e612f136e984f67e2
Jakob K <bluefireolymp@gmail.com>
2022-01-16 19:07:08 +0100

    Clean server world mixin in persistence module

--- fabrikmc-persistence/src/main/java/net/axay/fabrik/persistence/mixin/world/ServerWorldMixin.java
+++ fabrikmc-persistence/src/main/java/net/axay/fabrik/persistence/mixin/world/ServerWorldMixin.java
@@ -4,17 +4,8 @@ import net.axay.fabrik.persistence.CompoundProvider;
 import net.axay.fabrik.persistence.PersistentCompound;
 import net.axay.fabrik.persistence.PersistentCompoundImpl;
 import net.axay.fabrik.persistence.internal.CompoundPersistentState;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.PersistentStateManager;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.gen.Spawner;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.level.ServerWorldProperties;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,9 +13,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
-import java.util.concurrent.Executor;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin implements CompoundProvider {
@@ -34,7 +22,7 @@ public abstract class ServerWorldMixin implements CompoundProvider {
     @Shadow public abstract PersistentStateManager getPersistentStateManager();
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey<World> worldKey, DimensionType dimensionType, WorldGenerationProgressListener worldGenerationProgressListener, ChunkGenerator chunkGenerator, boolean debugWorld, long seed, List<Spawner> spawners, boolean shouldTickTime, CallbackInfo ci) {
+    private void onInit(CallbackInfo ci) {
         getPersistentStateManager().getOrCreate(
             nbt -> CompoundPersistentState.Companion.fromNbt(nbt, compound),
             () -> new CompoundPersistentState(compound),
