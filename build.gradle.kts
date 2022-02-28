389e94c473a86aedd88dea9a050db4827455bf4d
Jakob K <bluefireolymp@gmail.com>
2022-02-28 17:19:07 +0100

    Delete general uploadMod task

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,6 @@ allprojects {
 }
 
 tasks {
-    register("uploadMod") {
-        group = "upload"
-
-        val fabrikmcAllProject = project(":${rootProject.name}-all")
-
-        dependsOn(fabrikmcAllProject.tasks.named("uploadModrinth"))
-        dependsOn(fabrikmcAllProject.tasks.named("curseforge"))
-    }
-
     dokkaHtmlMultiModule {
         outputDirectory.set(projectDir.resolve("docs"))
 
