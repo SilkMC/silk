891a0f5f90b0c9f384a88e60d6bd4b0ab3709737
Jakob K <dev@jakobk.de>
2022-04-24 18:35:20 +0200

    Compile with Java 11 to support Kotlin 1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 allprojects {
     group = "net.axay"
-    version = "1.7.3"
+    version = "1.7.4"
     if (this.name.startsWith("fabrikmc")) {
         description = "FabrikMC is an API for using FabricMC with Kotlin"
     }
--- buildSrc/src/main/kotlin/java-version-script.gradle.kts
+++ buildSrc/src/main/kotlin/java-version-script.gradle.kts
@@ -14,14 +14,14 @@ repositories {
 tasks {
     withType<JavaCompile> {
         options.apply {
-            release.set(17)
+            release.set(11)
             encoding = "UTF-8"
         }
     }
 
     withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "17"
+            jvmTarget = "11"
         }
     }
 }
