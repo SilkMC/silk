9177eab63bf8b0942e9d8e336d80f9eb7c122823
timo <64286456+Hotkeyyy@users.noreply.github.com>
2022-05-03 21:12:42 +0200

    Update fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/player/runCommandExtension.kt
    Co-authored-by: Jakob K <dev@jakobk.de>

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/player/runCommandExtension.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/player/runCommandExtension.kt
@@ -1,6 +1,9 @@
 import net.axay.fabrik.core.Fabrik
 import net.minecraft.world.entity.player.Player
 
-fun Player.runCommand(command: String) {
+/**
+ * Executes the given [command] for this player.
+ */
+fun Player.performCommand(command: String) {
     Fabrik.currentServer!!.commands.performCommand(this.createCommandSourceStack(), command)
 }
