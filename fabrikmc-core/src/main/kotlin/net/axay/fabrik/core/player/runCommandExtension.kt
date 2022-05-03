324df0d61abe9be39ae71ec1b25ed9f259f0acee
Jakob K <dev@jakobk.de>
2022-05-03 21:14:58 +0200

    Rename to executeCommand and add missing question mark

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/player/runCommandExtension.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/player/runCommandExtension.kt
@@ -4,6 +4,6 @@ import net.minecraft.world.entity.player.Player
 /**
  * Executes the given [command] for this player.
  */
-fun Player.performCommand(command: String) {
-    Fabrik.currentServer?.commands.performCommand(this.createCommandSourceStack(), command)
+fun Player.executeCommand(command: String) {
+    Fabrik.currentServer?.commands?.performCommand(this.createCommandSourceStack(), command)
 }
