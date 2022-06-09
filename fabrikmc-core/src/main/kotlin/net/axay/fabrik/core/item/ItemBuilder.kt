2496031e80d48fa98b167cb0aefdf9caffa5bf22
Jakob K <dev@jakobk.de>
2022-06-09 17:29:26 +0200

    Improve setSkullTexture and remove dependency on nbt module from core

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/item/ItemBuilder.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/item/ItemBuilder.kt
@@ -2,20 +2,16 @@ package net.axay.fabrik.core.item
 
 import net.axay.fabrik.core.text.LiteralTextBuilder
 import net.axay.fabrik.core.text.literalText
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.ListTag
+import net.minecraft.nbt.NbtUtils
 import net.minecraft.nbt.StringTag
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.alchemy.Potion
 import net.minecraft.world.item.alchemy.PotionUtils
 import net.minecraft.world.level.ItemLike
-import net.axay.fabrik.nbt.dsl.nbtCompound
-import net.axay.fabrik.nbt.dsl.nbtList
-import net.axay.fabrik.nbt.toNbt
-import net.axay.fabrik.core.item.itemStack
-import net.minecraft.world.item.Items
-import net.minecraft.world.item.ItemStack
-import java.util.UUID
+import java.util.*
 
 /**
  * A utility to function for building more complex
@@ -44,20 +40,7 @@ fun ItemStack.setLore(text: Collection<Component>) {
  */
 inline fun ItemStack.setCustomName(baseText: String = "", builder: LiteralTextBuilder.() -> Unit = {}): ItemStack =
     setHoverName(literalText(baseText, builder))    
-    
-fun ItemStack.setSkullTexture(texture: String) {  
-    orCreateTag.put("SkullOwner", nbtCompound {
-        put("Id", UUID.randomUUID().toNbt())
-        put("Properties", nbtCompound {
-            put("textures", nbtList {
-                add(nbtCompound {
-                    put("Value", base64)
-                })
-            })
-        })
-    })
-}
-    
+
 /**
  * Sets the given potion for this [ItemStack].
  *
@@ -73,4 +56,38 @@ fun ItemStack.setSkullTexture(texture: String) {
  */
 fun ItemStack.setPotion(potion: Potion) {
     PotionUtils.setPotion(this, potion)
+}
+
+/**
+ * Configures the `SkullOwner` nbt tag to have the given [texture].
+ * The [texture] has to be base64 encoded.
+ *
+ * You can find a lot of heads with the associated base64 values on
+ * [minecraft-heads.com](https://minecraft-heads.com/).
+ *
+ * Optional, you can specify a [uuid]. This is *not* necessary if this head
+ * is just used because of its texture, but you should specify one if the head
+ * is associated with an actual player.
+ *
+ * ```kotlin
+ * skullStack.setSkullTexture(
+ *     texture = "eyJ0ZXh0dXJlcyI6ey...", // base64 encoded texture json (this example is truncated)
+ *     uuid = UUID.fromString("069a79f4-44e9-4726-a5be-fca90e38aaf5") // this is optional
+ * )
+ * ```
+ */
+fun ItemStack.setSkullTexture(
+    texture: String,
+    uuid: UUID = UUID(0, 0),
+) {
+    orCreateTag.put("SkullOwner", CompoundTag().apply {
+        put("Id", NbtUtils.createUUID(uuid))
+        put("Properties", CompoundTag().apply {
+            put("textures", ListTag().apply {
+                add(CompoundTag().apply {
+                    put("Value", StringTag.valueOf(texture))
+                })
+            })
+        })
+    })
 }
