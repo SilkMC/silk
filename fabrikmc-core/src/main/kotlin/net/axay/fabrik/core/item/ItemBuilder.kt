8573efb1c391b456bf32cc73ecf8537c75048fbb
Jakob K <dev@jakobk.de>
2022-06-10 22:35:26 +0200

    Improve item builder docs

--- fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/item/ItemBuilder.kt
+++ fabrikmc-core/src/main/kotlin/net/axay/fabrik/core/item/ItemBuilder.kt
@@ -7,6 +7,7 @@ import net.minecraft.nbt.ListTag
 import net.minecraft.nbt.NbtUtils
 import net.minecraft.nbt.StringTag
 import net.minecraft.network.chat.Component
+import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.alchemy.Potion
 import net.minecraft.world.item.alchemy.PotionUtils
@@ -14,8 +15,14 @@ import net.minecraft.world.level.ItemLike
 import java.util.*
 
 /**
- * A utility to function for building more complex
- * item stacks.
+ * A utility function for building complex [item] stacks with the given [amount].
+ *
+ * ```kotlin
+ * itemStack(Items.FEATHER, amount = 64) {
+ *     setCustomName("Cool Feather")
+ *     enchant(Enchantments.FIRE_ASPECT, 1)
+ * }
+ * ```
  */
 inline fun itemStack(
     item: ItemLike,
@@ -25,7 +32,8 @@ inline fun itemStack(
 
 /**
  * Sets the item lore, which is displayed below the display
- * name of the item stack.
+ * name of the item stack. Each element of the [text] collection represents
+ * one line.
  */
 fun ItemStack.setLore(text: Collection<Component>) {
     getOrCreateTagElement("display").put(
@@ -36,7 +44,7 @@ fun ItemStack.setLore(text: Collection<Component>) {
 
 /**
  * Opens a [LiteralTextBuilder] to change the custom name of
- * the item stack.
+ * the item stack. See [literalText].
  */
 inline fun ItemStack.setCustomName(baseText: String = "", builder: LiteralTextBuilder.() -> Unit = {}): ItemStack =
     setHoverName(literalText(baseText, builder))    
@@ -81,13 +89,16 @@ fun ItemStack.setSkullTexture(
     uuid: UUID = UUID(0, 0),
 ) {
     orCreateTag.put("SkullOwner", CompoundTag().apply {
-        put("Id", NbtUtils.createUUID(uuid))
-        put("Properties", CompoundTag().apply {
+        putUUID("Id", uuid)
+        getCompound("Properties").apply {
             put("textures", ListTag().apply {
                 add(CompoundTag().apply {
-                    put("Value", StringTag.valueOf(texture))
+                    putString("Value", texture)
                 })
             })
-        })
+        }
+    })
+}
+
     })
 }
