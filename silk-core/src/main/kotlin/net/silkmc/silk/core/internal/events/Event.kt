f00cf3cc5060cc28d966abc39cdce74450adae9c
Jakob K <dev@jakobk.de>
2022-06-21 20:58:22 +0200

    core: Extract event invoke logic to common companion object class

---
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/internal/events/Event.kt
@@ -0,0 +1,18 @@
+package net.silkmc.silk.core.internal.events
+
+import net.silkmc.silk.core.annotations.InternalSilkApi
+
+@InternalSilkApi
+open class Event<T> {
+    private val listeners = ArrayList<(T) -> Unit>()
+
+    fun register(callback: (T) -> Unit) {
+        listeners += callback
+    }
+
+    fun invoke(instance: T) {
+        listeners.forEach {
+            it(instance)
+        }
+    }
+}
--- silk-core/src/main/kotlin/net/silkmc/silk/core/internal/events/ServerEvents.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/internal/events/ServerEvents.kt
@@ -6,17 +6,6 @@ import net.silkmc.silk.core.annotations.InternalSilkApi
 @InternalSilkApi
 class ServerEvents {
     class Init(val server: MinecraftServer) {
-        companion object {
-            private val listeners = ArrayList<(Init) -> Unit>()
-            fun register(callback: (Init) -> Unit) {
-                listeners += callback
-            }
-            fun invoke(server: MinecraftServer) {
-                val init = Init(server)
-                listeners.forEach {
-                    it(init)
-                }
-            }
-        }
+        companion object : Event<Init>()
     }
 }
