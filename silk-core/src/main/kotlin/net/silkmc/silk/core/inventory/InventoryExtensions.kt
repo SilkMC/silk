9b19d36aae7873bf7572ee6702a81136071d1e62
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-06-23 20:24:13 +0200

    Create InventoryExtensions.kt

---
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/inventory/InventoryExtensions.kt
@@ -0,0 +1,13 @@
+package net.silkmc.silk.core.inventory
+
+import net.minecraft.world.Container
+import net.minecraft.world.item.ItemStack
+
+fun Container.canAddItem(stack: ItemStack): Boolean{
+    if(this.isEmpty)return true
+    for (i in 0 .. this.containerSize){
+        if (this.getItem(i).isEmpty)return true
+        if (this.getItem(i).isStackable && this.getItem(i).item == stack.item && this.getItem(i).count < this.getItem(i).maxStackSize)return true
+    }
+    return false
+}
