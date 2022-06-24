ea7e9c25b484327241cbc1eaf0b62eeaae56b1fe
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-06-24 13:52:49 +0200

    reformat code

--- silk-core/src/main/kotlin/net/silkmc/silk/core/inventory/InventoryExtensions.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/inventory/InventoryExtensions.kt
@@ -3,11 +3,14 @@ package net.silkmc.silk.core.inventory
 import net.minecraft.world.Container
 import net.minecraft.world.item.ItemStack
 
-fun Container.canAddItem(stack: ItemStack): Boolean{
-    if(this.isEmpty)return true
-    for (i in 0 .. this.containerSize){
-        if (this.getItem(i).isEmpty)return true
-        if (this.getItem(i).isStackable && this.getItem(i).item == stack.item && this.getItem(i).count + stack.count <= this.getItem(i).maxStackSize)return true
+fun Container.canAddItem(stack: ItemStack): Boolean {
+    if (this.isEmpty) return true
+    for (i in 0..this.containerSize) {
+        if (this.getItem(i).isEmpty) return true
+        if (this.getItem(i).isStackable && this.getItem(i).item == stack.item && this.getItem(i).count + stack.count <= this.getItem(
+                i
+            ).maxStackSize
+        ) return true
     }
     return false
 }
