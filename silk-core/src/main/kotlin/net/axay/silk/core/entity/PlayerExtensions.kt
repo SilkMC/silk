6cf25d2a62d36717a61d6823bd95e4bda2b08d1a
Jakob K <dev@jakobk.de>
2022-06-24 03:46:27 +0200

    reformat server executeCommand extension

--- silk-core/src/main/kotlin/net/axay/silk/core/entity/PlayerExtensions.kt
+++ silk-core/src/main/kotlin/net/axay/silk/core/entity/PlayerExtensions.kt
@@ -4,6 +4,8 @@ import net.minecraft.world.entity.player.Player
 
 /**
  * Executes the given [command] for this player.
+ *
+ * Note that the [command] must not contain the slash prefix.
  */
 fun Player.executeCommand(command: String) {
     server?.commands?.performCommand(createCommandSourceStack(), command)
--- silk-core/src/main/kotlin/net/axay/silk/core/server/ServerExtensions.kt
+++ silk-core/src/main/kotlin/net/axay/silk/core/server/ServerExtensions.kt
@@ -4,8 +4,9 @@ import net.minecraft.server.MinecraftServer
 
 /**
  * Executes the given [command] for this server.
+ *
+ * Note that the [command] must not contain the slash prefix.
  */
-
-fun MinecraftServer.executeCommand(command: String){
-    this.commands.performCommand(this.createCommandSourceStack(), command)
+fun MinecraftServer.executeCommand(command: String) {
+    commands.performCommand(createCommandSourceStack(), command)
 }
