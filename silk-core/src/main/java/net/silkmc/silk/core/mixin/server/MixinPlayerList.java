ba62b7a4a985cefe0b2a8150ccdc082052e9a7ba
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-31 13:19:43 +0200

    Make player join event working
    didn't have motivation but now it works

--- silk-core/src/main/java/net/silkmc/silk/core/mixin/server/MixinPlayerList.java
+++ silk-core/src/main/java/net/silkmc/silk/core/mixin/server/MixinPlayerList.java
@@ -1,37 +1,41 @@
 package net.silkmc.silk.core.mixin.server;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import net.minecraft.server.players.GameProfileCache;
 import net.minecraft.server.players.PlayerList;
-import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.storage.LevelData;
+import net.silkmc.silk.core.event.PlayerConnectionEvents;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.Optional;
 
 @Mixin(PlayerList.class)
 public class MixinPlayerList {
 
-    @Inject(
+    @Unique private ServerPlayer silk$temporaryPlayer = null;
+
+
+    @Inject(method = "placeNewPlayer", at = @At("HEAD"))
+    private void onPlayerJoin(Connection connection, ServerPlayer player, CallbackInfo ci) {
+        silk$temporaryPlayer = player;
+    }
+
+    @ModifyArg(
             method = "placeNewPlayer",
             at = @At(
-                value = "INVOKE",
-                target = "Lnet/minecraft/server/players/PlayerList;broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/resources/ResourceKey;)V"
-            ),
-            locals = LocalCapture.CAPTURE_FAILSOFT
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/players/PlayerList;broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/resources/ResourceKey;)V"
+            )
     )
-    private void onPlayerJoin(Connection connection, ServerPlayer player, CallbackInfo ci, GameProfile gameProfile, GameProfileCache gameProfileCache, Optional optional, String string, CompoundTag compoundTag, ResourceKey resourceKey, ServerLevel serverLevel, ServerLevel serverLevel2, String string2, LevelData levelData, ServerGamePacketListenerImpl serverGamePacketListenerImpl, GameRules gameRules, boolean bl, boolean bl2, MutableComponent mutableComponent) {
-
+    private Component changeJoinMessage(Component message) {
+        Component component;
+        PlayerConnectionEvents.PlayerJoinEvent event = new PlayerConnectionEvents.PlayerJoinEvent(silk$temporaryPlayer, message);
+        PlayerConnectionEvents.INSTANCE.getPlayerJoin().invoke(event);
+        component = event.getJoinMessage();
+        silk$temporaryPlayer = null;
+        return component;
     }
 }
--- silk-core/src/main/kotlin/net/silkmc/silk/core/event/PlayerConnectionEvents.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/event/PlayerConnectionEvents.kt
@@ -1,6 +1,6 @@
 package net.silkmc.silk.core.event
 
-import net.minecraft.network.Connection
+import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerPlayer
 import net.silkmc.silk.core.annotations.ExperimentalSilkApi
 
@@ -13,6 +13,16 @@ val Events.PlayerConnection get() = PlayerConnectionEvents
 @ExperimentalSilkApi
 object PlayerConnectionEvents {
 
-    open class PlayerConnectionEvent(val server: ServerPlayer, val connection: Connection)
-    class PlayerJoinEvent()
+    class PlayerJoinEvent(val player: ServerPlayer, var joinMessage: Component)
+    class PlayerQuitEvent(val player: ServerPlayer, var quitMessage: Component)
+
+    /**
+     * Called when a player joins the server
+     */
+    val playerJoin = Event.syncAsync<PlayerJoinEvent, EventScope.Empty> { EventScope.Empty }
+
+    /**
+     * Called when a player leaves the server
+     */
+    val playerQuit = Event.syncAsync<PlayerQuitEvent, EventScope.Empty> { EventScope.Empty }
 }
