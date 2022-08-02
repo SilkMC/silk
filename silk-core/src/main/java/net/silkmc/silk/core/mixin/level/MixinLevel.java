5681d43776fa339f38f5c63240028bbbddbea5cd
mooziii <63669478+mooziii@users.noreply.github.com>
2022-08-02 09:30:56 +0200

    Send inventory update

--- silk-core/src/main/java/net/silkmc/silk/core/mixin/level/MixinLevel.java
+++ silk-core/src/main/java/net/silkmc/silk/core/mixin/level/MixinLevel.java
@@ -1,9 +1,11 @@
 package net.silkmc.silk.core.mixin.level;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.silkmc.silk.core.Silk;
 import net.silkmc.silk.core.event.BlockEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,9 +23,11 @@ public abstract class MixinLevel {
         BlockEvents.BlockEvent blockEvent = new BlockEvents.BlockEvent((Level) (Object) this, pos, state);
         if (BlockEvents.INSTANCE.getSet().invoke(blockEvent).isCancelled().get()) {
             cir.setReturnValue(false);
+            for (ServerPlayer player : Silk.INSTANCE.getCurrentServer().getPlayerList().getPlayers()) {
+                player.containerMenu.sendAllDataToRemote(); // send inventory update to every player as we can't get the specific one from here
+            }
         }
     }
-
 
     @Inject(method = "destroyBlock", at = @At("HEAD"), cancellable = true)
     private void onDestroyBlock(BlockPos pos, boolean drop, Entity breakingEntity, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir) {
