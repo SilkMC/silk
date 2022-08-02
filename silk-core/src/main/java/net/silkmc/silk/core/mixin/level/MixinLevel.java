c2698baad9ee0db358b146c5d562df31dae3a966
mooziii <63669478+mooziii@users.noreply.github.com>
2022-08-02 09:20:57 +0200

    Create MixinLevel.java

---
+++ silk-core/src/main/java/net/silkmc/silk/core/mixin/level/MixinLevel.java
@@ -0,0 +1,35 @@
+package net.silkmc.silk.core.mixin.level;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.silkmc.silk.core.event.BlockEvents;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Level.class)
+public abstract class MixinLevel {
+
+    @Shadow public abstract BlockState getBlockState(BlockPos pos);
+
+    @Inject(method = "setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;II)Z", at = @At("HEAD"), cancellable = true)
+    private void onSetBlock(BlockPos pos, BlockState state, int flags, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir) {
+        BlockEvents.BlockEvent blockEvent = new BlockEvents.BlockEvent((Level) (Object) this, pos, state);
+        if (BlockEvents.INSTANCE.getSet().invoke(blockEvent).isCancelled().get()) {
+            cir.setReturnValue(false);
+        }
+    }
+
+
+    @Inject(method = "destroyBlock", at = @At("HEAD"), cancellable = true)
+    private void onDestroyBlock(BlockPos pos, boolean drop, Entity breakingEntity, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir) {
+        BlockEvents.BlockEvent blockEvent = new BlockEvents.BlockEvent((Level) (Object) this, pos, getBlockState(pos));
+        if (BlockEvents.INSTANCE.getDestroy().invoke(blockEvent).isCancelled().get()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
