b5653531023909d747e08a25f5def319c64067ab
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-08-04 00:53:21 +0200

    create player item events

---
+++ silk-core/src/main/java/net/silkmc/silk/core/mixin/player/MixinInventory.java
@@ -0,0 +1,31 @@
+package net.silkmc.silk.core.mixin.player;
+
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.silkmc.silk.core.event.player.PlayerItemEvents;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Inventory.class)
+public class MixinInventory {
+
+    @Inject(method = "add(Lnet/minecraft/world/item/ItemStack;)Z", at = @At(value = "RETURN"))
+    public void onCollectItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir){
+        var player = (Player) (Object) this;
+        PlayerItemEvents.INSTANCE.getItemCollect().invoke(new PlayerItemEvents.PlayerItemEvent(stack, player.level, player));
+    }
+
+    @Inject(method = "add(Lnet/minecraft/world/item/ItemStack;)Z", at = @At(value = "HEAD"), cancellable = true)
+    public void onPreCollectItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir){
+        var player = (Player) (Object) this;
+        var event = PlayerItemEvents.INSTANCE.getPreItemCollect().invoke(new PlayerItemEvents.PlayerItemEvent(stack, player.level, player));
+        if (event.isCancelled().get()) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+
+}
---
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/event/player/PlayerItemEvents.kt
@@ -0,0 +1,28 @@
+package net.silkmc.silk.core.event.player
+
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import net.silkmc.silk.core.event.Event
+import net.silkmc.silk.core.event.EventScope
+
+/**
+ * Events related to Items and [net.minecraft.world.entity.player.Player]s
+ */
+val PlayerEvents.Item get() = PlayerItemEvents
+
+object PlayerItemEvents {
+
+    class PlayerItemEvent(val item: ItemStack, level: Level, player: Player) : PlayerEvents.PlayerEvent(player)
+
+    /**
+     * Called before an item gets collected
+     */
+    val preItemCollect = Event.syncAsync<PlayerItemEvent, EventScope.Cancellable> { EventScope.Cancellable() }
+
+    /**
+     * Called after an item gets collected
+     */
+    val itemCollect = Event.syncAsync<PlayerItemEvent, EventScope.Empty> { EventScope.Empty }
+
+}
