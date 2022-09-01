c55d7e4f23f1c91ddd0e19d0f018c7b606e616bd
Anton <anton.wiblishauser@gmx.de>
2022-09-01 21:33:17 +0200

    use correct codestyle

--- silk-core/src/main/java/net/silkmc/silk/core/mixin/player/MixinInventory.java
+++ silk-core/src/main/java/net/silkmc/silk/core/mixin/player/MixinInventory.java
@@ -4,7 +4,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.silkmc.silk.core.event.player.PlayerItemEvents;
+import net.silkmc.silk.core.event.PlayerEvents;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,10 +19,12 @@ public class MixinInventory {
     @Final
     public Player player;
 
-    @Inject(method = "add(Lnet/minecraft/world/item/ItemStack;)Z", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(
+        method = "add(Lnet/minecraft/world/item/ItemStack;)Z",
+        at = @At(value = "HEAD"), cancellable = true
+    )
     public void onPreCollectItem(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
-        var event = PlayerItemEvents.INSTANCE.getPreItemCollect().invoke(new PlayerItemEvents.ServerPlayerItemEvent(itemStack, (ServerPlayer) player));
-        if (event.isCancelled().get()) {
+        if (PlayerEvents.INSTANCE.getPreItemCollect().invoke(new PlayerEvents.ServerPlayerItemEvent(itemStack, (ServerPlayer) player)).isCancelled().get()) {
             cir.setReturnValue(false);
             cir.cancel();
         }
--- silk-core/src/main/java/net/silkmc/silk/core/mixin/player/MixinServerPlayer.java
+++ silk-core/src/main/java/net/silkmc/silk/core/mixin/player/MixinServerPlayer.java
@@ -3,7 +3,7 @@ package net.silkmc.silk.core.mixin.player;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
-import net.silkmc.silk.core.event.player.PlayerItemEvents;
+import net.silkmc.silk.core.event.PlayerEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ServerPlayer.class)
 public class MixinServerPlayer {
 
-    @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("HEAD"))
+    @Inject(
+        method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;",
+        at = @At("HEAD")
+    )
     public void onDrop(ItemStack itemStack, boolean bl, boolean bl2, CallbackInfoReturnable<ItemEntity> cir) {
-        var player = (ServerPlayer) (Object) this;
-        var event = PlayerItemEvents.INSTANCE.getPreItemDrop().invoke(new PlayerItemEvents.ServerPlayerItemEvent(itemStack, player));
-        if (event.isCancelled().get()) cir.cancel();
+        if (PlayerEvents.INSTANCE.getPreItemDrop().invoke(new PlayerEvents.ServerPlayerItemEvent(itemStack, (ServerPlayer) (Object) this)).isCancelled().get())
+            cir.cancel();
     }
 
 }
