87c6789b181ce60f9354c563d2005c4c06f7ac69
Anton <anton.wiblishauser@gmx.de>
2022-09-01 21:33:00 +0200

    merge player item and player events

--- silk-core/src/main/kotlin/net/silkmc/silk/core/event/PlayerEvents.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/event/PlayerEvents.kt
@@ -1,8 +1,10 @@
-package net.silkmc.silk.core.event.player
+package net.silkmc.silk.core.event
 
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.item.ItemStack
 import net.silkmc.silk.core.annotations.ExperimentalSilkApi
 import net.silkmc.silk.core.event.Event
+import net.silkmc.silk.core.event.EventScope
 import net.silkmc.silk.core.event.Events
 
 @Suppress("UnusedReceiverParameter") // receiver is for namespacing only
@@ -22,4 +24,18 @@ object PlayerEvents {
      * Called after a player has received all login information from the server.
      */
     val postLogin = Event.syncAsyncImmutable<ServerPlayerEvent>()
+
+
+    class ServerPlayerItemEvent(val item: ItemStack, player: ServerPlayer) : PlayerEvents.ServerPlayerEvent(player)
+
+    /**
+     * Called before an item gets collected by a player
+     */
+    val preItemCollect = Event.syncAsync<ServerPlayerItemEvent, EventScope.Cancellable> { EventScope.Cancellable() }
+
+    /**
+     * Called before an item gets dropped by a player
+     */
+    val preItemDrop = Event.syncAsync<ServerPlayerItemEvent, EventScope.Cancellable> { EventScope.Cancellable() }
+
 }
