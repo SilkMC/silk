d3706e319a4e0ad73bb05fe850eebef78a6d175e
Anton <anton.wiblishauser@gmx.de>
2022-09-14 17:36:15 +0200

    fix null tablist

--- silk-game/src/main/java/net/silkmc/silk/game/mixin/MixinServerPlayer.java
+++ silk-game/src/main/java/net/silkmc/silk/game/mixin/MixinServerPlayer.java
@@ -5,18 +5,20 @@ import net.minecraft.server.level.ServerPlayer;
 import net.silkmc.silk.game.tablist.Tablist;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayer.class)
 public class MixinServerPlayer {
 
     ServerPlayer player = (ServerPlayer) (Object) this;
 
-    @Redirect(method = "getTabListDisplayName", at = @At(value = "HEAD"))
-    public Component onGetTablistName() {
+    @Inject(method = "getTabListDisplayName", at = @At("RETURN"), cancellable = true)
+    public void onGetTablistName(CallbackInfoReturnable<Component> cir) {
         Tablist tablist = Tablist.getCurrentTablist();
-        if (tablist == null) return null;
-        return tablist.getPlayerNames().get(player.getUUID());
+        if (tablist == null) return;
+        cir.setReturnValue(tablist.getPlayerNames().get(player.getUUID()));
+        cir.cancel();
     }
 
 }
