cd09ebbe2ab532336e02daddb33fb14fa9a73d78
Jakob K <dev@jakobk.de>
2022-09-21 21:27:16 +0200

    game: Improve sideboard implementation and concurrency

--- silk-game/src/main/kotlin/net/silkmc/silk/game/sideboard/internal/SideboardScoreboard.kt
+++ silk-game/src/main/kotlin/net/silkmc/silk/game/sideboard/internal/SideboardScoreboard.kt
@@ -1,5 +1,7 @@
 package net.silkmc.silk.game.sideboard.internal
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
 import net.minecraft.network.chat.Component
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.game.ClientboundSetDisplayObjectivePacket
@@ -8,12 +10,17 @@ import net.minecraft.network.protocol.game.ClientboundSetPlayerTeamPacket
 import net.minecraft.network.protocol.game.ClientboundSetScorePacket
 import net.minecraft.server.ServerScoreboard
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.scores.Objective
 import net.minecraft.world.scores.PlayerTeam
-import net.minecraft.world.scores.Score
 import net.minecraft.world.scores.Scoreboard
 import net.minecraft.world.scores.criteria.ObjectiveCriteria
 import net.silkmc.silk.core.annotations.InternalSilkApi
-import net.silkmc.silk.core.packet.sendPacket
+import net.silkmc.silk.core.event.Events
+import net.silkmc.silk.core.event.Player
+import net.silkmc.silk.core.kotlin.LimitedAccessWrapper
+import net.silkmc.silk.core.task.silkCoroutineScope
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * A server side scoreboard which is only displayed to
@@ -31,78 +38,138 @@ import net.silkmc.silk.core.packet.sendPacket
 class SideboardScoreboard(
     name: String,
     displayName: Component,
-) : Scoreboard() {
+) {
+
     companion object {
-        private val sidebarId = getDisplaySlotByName("sidebar")
-    }
+        /**
+         * The ID of the sideboard display slot.
+         */
+        private val sidebarId = Scoreboard.getDisplaySlotByName("sidebar")
 
-    private val players = HashSet<ServerPlayer>()
-
-    private val dummyObjective =
-        addObjective(name, ObjectiveCriteria.DUMMY, displayName, ObjectiveCriteria.RenderType.INTEGER)
-
-    init {
-        setDisplayObjective(sidebarId, dummyObjective)
-    }
-
-    fun displayToPlayer(player: ServerPlayer) {
-        players += player
-
-        val updatePackets = ArrayList<Packet<*>>()
-
-        updatePackets += ClientboundSetObjectivePacket(dummyObjective, ClientboundSetObjectivePacket.METHOD_ADD)
-        updatePackets += ClientboundSetDisplayObjectivePacket(sidebarId, dummyObjective)
-        getPlayerScores(dummyObjective).mapTo(updatePackets) {
-            ClientboundSetScorePacket(
-                ServerScoreboard.Method.CHANGE, it.objective!!.name, it.owner, it.score
-            )
-        }
-        playerTeams.mapTo(updatePackets) { ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(it, true) }
-
-        updatePackets.forEach(player.connection::send)
-    }
-
-    fun hideFromPlayer(player: ServerPlayer) {
-        players -= players
-
-        player.connection.send(ClientboundSetObjectivePacket(dummyObjective, ClientboundSetObjectivePacket.METHOD_REMOVE))
-    }
-
-    fun setPlayerScore(player: String, score: Int) {
-        getOrCreatePlayerScore(player, dummyObjective).score = score
-    }
-
-    override fun onScoreChanged(score: Score) {
-        super.onScoreChanged(score)
-
-        if (dummyObjective == score.objective) {
-            players.sendPacket(
-                ClientboundSetScorePacket(
-                    ServerScoreboard.Method.CHANGE, score.objective!!.name, score.owner, score.score
-                )
-            )
+        private val playerBoards = ConcurrentHashMap<UUID, SideboardScoreboard>().also { map ->
+            Events.Player.preQuit.listen { event ->
+                silkCoroutineScope.launch {
+                    map.remove(event.player.uuid)?.hideFromPlayer(event.player, sendRemove = false)
+                }
+            }
         }
     }
 
-    override fun addPlayerToTeam(playerName: String, team: PlayerTeam): Boolean {
-        return if (super.addPlayerToTeam(playerName, team)) {
-            players.sendPacket(ClientboundSetPlayerTeamPacket.createPlayerPacket(team, playerName, ClientboundSetPlayerTeamPacket.Action.ADD))
-            true
-        } else false
+    private val dummyObjective = Objective(NoopScoreboard, name, ObjectiveCriteria.DUMMY, displayName, ObjectiveCriteria.RenderType.INTEGER)
+
+    // scoreboard state
+    private val lines = LimitedAccessWrapper(ArrayList<Line>())
+
+    // for sending state to player
+    private val packetFlow = MutableSharedFlow<Packet<*>>()
+    private val playerJobs = ConcurrentHashMap<UUID, Job>()
+
+    /**
+     * See [net.silkmc.silk.game.sideboard.Sideboard.displayToPlayer].
+     */
+    suspend fun displayToPlayer(player: ServerPlayer) {
+        // already start to create the init packets in the background
+        val initPacketsDeferred = silkCoroutineScope.async {
+            buildList {
+                // send required dummy objective
+                add(ClientboundSetObjectivePacket(dummyObjective, ClientboundSetObjectivePacket.METHOD_ADD))
+
+                // add line packets
+                lines.access {
+                    it.forEach { line ->
+                        this@buildList.addAll(line.createInitPackets(0))
+                    }
+                }
+
+                // display at the side
+                add(ClientboundSetDisplayObjectivePacket(sidebarId, dummyObjective))
+            }
+        }
+
+        // remove the previous board
+        playerBoards.put(player.uuid, this)
+            ?.hideFromPlayer(player)
+
+        val packetJob = silkCoroutineScope.launch(start = CoroutineStart.LAZY) {
+            initPacketsDeferred.await().forEach(player.connection::send)
+            packetFlow.collect(player.connection::send)
+        }
+
+        playerJobs.put(player.uuid, packetJob)
+            // shouldn't be necessary, but leaving this here to really make sure there are no competing jobs
+            ?.cancelAndJoin()
+
+        packetJob.start()
     }
 
-    override fun removePlayerFromTeam(playerName: String, team: PlayerTeam) {
-        super.removePlayerFromTeam(playerName, team)
-        players.sendPacket(ClientboundSetPlayerTeamPacket.createPlayerPacket(team, playerName, ClientboundSetPlayerTeamPacket.Action.REMOVE))
+    /**
+     * See [net.silkmc.silk.game.sideboard.Sideboard.hideFromPlayer].
+     */
+    suspend fun hideFromPlayer(player: ServerPlayer, sendRemove: Boolean = true) {
+        // cancel packet collector job
+        playerJobs.remove(player.uuid)?.cancelAndJoin()
+        // might already be removed, but make sure it is
+        playerBoards.remove(player.uuid, this)
+
+        if (sendRemove) {
+            lines.access {
+                it.forEach { line ->
+                    line.createRemovePackets().forEach(player.connection::send)
+                }
+            }
+            player.connection.send(ClientboundSetObjectivePacket(dummyObjective, ClientboundSetObjectivePacket.METHOD_REMOVE))
+        }
     }
 
-    override fun onTeamAdded(team: PlayerTeam) {
-        super.onTeamAdded(team)
-        players.sendPacket(ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(team, true))
+    private fun emitPacket(packet: Packet<*>) {
+        silkCoroutineScope.launch {
+            packetFlow.emit(packet)
+        }
     }
 
-    override fun onTeamChanged(team: PlayerTeam) {
-        super.onTeamChanged(team)
-        players.sendPacket(ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(team, false))
+    inner class Line(number: Int) {
+        private val fakePlayerName = number.toString().map { "§${it}" }.joinToString("")
+        private val teamName = "team_${number}"
+
+        private val unsafeTeam = PlayerTeam(NoopScoreboard, teamName)
+        private val wrappedTeam = LimitedAccessWrapper(unsafeTeam)
+
+        suspend fun setContent(text: Component) {
+            val packet: Packet<*>
+            wrappedTeam.access { team ->
+                team.playerPrefix = text
+                packet = ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(team, false)
+            }
+            emitPacket(packet)
+        }
+
+        suspend fun createInitPackets(score: Int): List<Packet<*>> {
+            return buildList {
+                add(ClientboundSetScorePacket(ServerScoreboard.Method.CHANGE, dummyObjective.name, fakePlayerName, score))
+                wrappedTeam.access { team ->
+                    add(ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(team, true))
+                }
+                add(ClientboundSetPlayerTeamPacket.createPlayerPacket(unsafeTeam, fakePlayerName, ClientboundSetPlayerTeamPacket.Action.ADD))
+            }
+        }
+
+        fun createRemovePackets(): List<Packet<*>> {
+            return buildList {
+                add(ClientboundSetScorePacket(ServerScoreboard.Method.REMOVE, dummyObjective.name, fakePlayerName, 0))
+                add(ClientboundSetPlayerTeamPacket.createRemovePacket(unsafeTeam))
+            }
+        }
+    }
+
+    suspend fun addLine(): Line {
+        val line: Line
+        lines.access {
+            val index = it.lastIndex + 1
+            line = Line(index)
+            it.add(line)
+        }
+        line.createInitPackets(0)
+            .forEach(::emitPacket)
+        return line
     }
 }
