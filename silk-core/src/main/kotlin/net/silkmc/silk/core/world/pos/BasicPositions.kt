5fa3e05100b495b86df0ba7484bb2fc33076b5fc
Jakob K <dev@jakobk.de>
2022-09-27 15:29:21 +0200

    core: Make Pos2i and Pos3i serializable

--- silk-core/src/main/kotlin/net/silkmc/silk/core/world/pos/BasicPositions.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/world/pos/BasicPositions.kt
@@ -1,5 +1,6 @@
 package net.silkmc.silk.core.world.pos
 
+import kotlinx.serialization.Serializable
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Vec3i
 
@@ -7,6 +8,7 @@ import net.minecraft.core.Vec3i
  * A simple class representing **a pair of x and z**. As y would be the height
  * in Minecraft, this two-dimensional class does not contain a y value.
  */
+@Serializable
 data class Pos2i(val x: Int, val z: Int) {
     fun toPair() = x to z
     fun toBlockPos(y: Int) = BlockPos(x, y, z)
@@ -15,6 +17,7 @@ data class Pos2i(val x: Int, val z: Int) {
 /**
  * A class representing **a triple of x, y and z**, where y is the height in Minecraft.
  */
+@Serializable
 data class Pos3i(val x: Int, val y: Int, val z: Int) {
     fun toTriple() = Triple(x, y, z)
     fun toVec3i() = Vec3i(x, y, z)
