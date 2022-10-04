f9132f1db793f7d05680a322a07d0f177ddc1f79
SettingDust <settingdust@gmail.com>
2022-10-04 18:31:29 +0800

    test(nbt): test for serializers

---
+++ silk-nbt/src/test/kotlin/net/silkmc/silk/nbt/NbtSerializerTest.kt
@@ -0,0 +1,79 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
+package net.silkmc.silk.nbt
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.ExperimentalSerializationApi
+import net.minecraft.nbt.Tag
+import net.silkmc.silk.core.serialization.SilkSerializer
+import net.silkmc.silk.nbt.dsl.nbtCompound
+import net.silkmc.silk.nbt.serialization.Nbt
+import net.silkmc.silk.nbt.serialization.serializer.*
+
+private val nbt = Nbt {}
+
+private inline fun <reified Data, T : Tag, Serializer : SilkSerializer<T>> Data.shouldSerializable(
+    serializer: Serializer,
+    tag: Data.() -> T
+) = tag(this).let { nbt.encodeToNbtElement(serializer, it) shouldBe it }
+
+
+private inline fun <reified Data, T : Tag, Deserializer : SilkSerializer<T>> Data.shouldDeserializable(
+    deserializer: Deserializer,
+    tag: Data.() -> T
+) = tag(this).let { nbt.decodeFromNbtElement(deserializer, it) shouldBe it }
+
+class NbtSerializerTest : StringSpec({
+    "primitive should deserializable" {
+        "foo".shouldDeserializable(StringTagSerializer) { toNbt() }
+        25565.toByte().shouldDeserializable(ByteTagSerializer) { toNbt() }
+        25565.toDouble().shouldDeserializable(DoubleTagSerializer) { toNbt() }
+        25565F.shouldDeserializable(FloatTagSerializer) { toNbt() }
+        25565.shouldDeserializable(IntTagSerializer) { toNbt() }
+        25565L.shouldDeserializable(LongTagSerializer) { toNbt() }
+        25565.toShort().shouldDeserializable(ShortTagSerializer) { toNbt() }
+    }
+
+    "primitive should serializable" {
+        "foo".shouldSerializable(StringTagSerializer) { toNbt() }
+        25565.toByte().shouldSerializable(ByteTagSerializer) { toNbt() }
+        25565.toDouble().shouldSerializable(DoubleTagSerializer) { toNbt() }
+        25565F.shouldSerializable(FloatTagSerializer) { toNbt() }
+        25565.shouldSerializable(IntTagSerializer) { toNbt() }
+        25565L.shouldSerializable(LongTagSerializer) { toNbt() }
+        25565.toShort().shouldSerializable(ShortTagSerializer) { toNbt() }
+    }
+
+    "collection like should serializable" {
+        byteArrayOf(0, 2, 4, 8).shouldSerializable(ByteArrayTagSerializer) { toNbt() }
+        intArrayOf(0, 1, 2, 3).shouldSerializable(IntArrayTagSerializer) { toNbt() }
+        longArrayOf(5, 6, 7, 8).shouldSerializable(LongArrayTagSerializer) { toNbt() }
+        listOf("foo", "bar").shouldSerializable(ListTagSerializer) { map { it.toNbt() }.toNbt() }
+    }
+
+    "collection like should deserializable" {
+        byteArrayOf(0, 2, 4, 8).shouldDeserializable(ByteArrayTagSerializer) { toNbt() }
+        intArrayOf(0, 1, 2, 3).shouldDeserializable(IntArrayTagSerializer) { toNbt() }
+        longArrayOf(5, 6, 7, 8).shouldDeserializable(LongArrayTagSerializer) { toNbt() }
+        listOf("foo", "bar").shouldDeserializable(ListTagSerializer) { map { it.toNbt() }.toNbt() }
+    }
+
+    "compound should serializable" {
+        nbtCompound {
+            put("foo", 0)
+            compound("bar") {
+                put("xxx", "abb")
+            }
+        }.shouldSerializable(CompoundTagSerializer) { this }
+    }
+
+    "compound should deserializable" {
+        nbtCompound {
+            put("foo", 0)
+            compound("bar") {
+                put("xxx", "abb")
+            }
+        }.shouldDeserializable(CompoundTagSerializer) { this }
+    }
+})
