78ae41bcc45ed53974dc8ed5408bf0950bcd6ca6
Jakob K <dev@jakobk.de>
2022-12-07 17:15:36 +0100

    Update to full 1.19.3 release

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -17,11 +17,10 @@ object BuildConstants {
     const val majorMinecraftVersion = "1.19"
 
     // check these values here: https://axay.net/mcdev
-    const val minecraftVersion = "1.19.3-pre2"
-    const val quiltMappingsVersion = "${minecraftVersion}+build.1:v2"
+    const val minecraftVersion = "1.19.3"
     const val parchmentMappingsVersion = "1.19.2:2022.11.20"
-    const val fabricLoaderVersion = "0.14.10"
-    const val fabricApiVersion = "0.67.2+1.19.3"
+    const val fabricLoaderVersion = "0.14.11"
+    const val fabricApiVersion = "0.68.1+1.19.3"
     const val fabricLanguageKotlinVersion = "1.8.6+kotlin.1.7.21"
 
     const val kotestVersion = "5.5.4"
