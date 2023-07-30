e81bf350bdeb3719fd03fdbc465f02e975a48abf
Jakob K <dev@jakobk.de>
2023-07-30 18:31:28 +0200

    repo: Update dependencies and Gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,16 +14,16 @@ repositories {
 dependencies {
     fun pluginDep(id: String, version: String) = "${id}:${id}.gradle.plugin:${version}"
 
-    val kotlinVersion = "1.8.21"
+    val kotlinVersion = "1.9.0"
 
     compileOnly(kotlin("gradle-plugin", embeddedKotlinVersion))
     runtimeOnly(kotlin("gradle-plugin", kotlinVersion))
     compileOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", embeddedKotlinVersion))
     runtimeOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", kotlinVersion))
 
-    implementation(pluginDep("fabric-loom", "1.2-SNAPSHOT"))
-    implementation(pluginDep("io.github.juuxel.loom-quiltflower", "1.10.0"))
-    implementation(pluginDep("com.modrinth.minotaur", "2.7.5"))
+    implementation(pluginDep("fabric-loom", "1.3-SNAPSHOT"))
+    implementation(pluginDep("io.github.juuxel.loom-vineflower", "1.11.0"))
+    implementation(pluginDep("com.modrinth.minotaur", "2.8.2"))
 
     implementation(pluginDep("io.papermc.paperweight.userdev", "1.5.5"))
     implementation(pluginDep("xyz.jpenilla.run-paper", "2.1.0"))
--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -16,10 +16,8 @@ object BuildConstants {
 
     // check these values here: https://jakobk.net/mcdev
     const val minecraftVersion = "1.20.1"
-    const val parchmentMappingsVersion = "1.19.3:2023.03.12"
     const val fabricLoaderVersion = "0.14.21"
-    const val fabricApiVersion = "0.83.1+1.20.1"
-    const val fabricLanguageKotlinVersion = "1.9.4+kotlin.1.8.21"
+    const val fabricLanguageKotlinVersion = "1.10.8+kotlin.1.9.0"
 
     const val kotestVersion = "5.6.2"
     const val mockkVersion = "1.13.5"
