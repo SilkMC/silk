26412f6d8f175c730bc05978252fa19a8b8208cc
Jakob K <dev@jakobk.de>
2024-04-24 23:05:47 +0200

    core: Remove joml vector extensions
    (they have been accidentally introduced when mojang removed their
    own Vector3f)

--- silk-core/src/main/kotlin/net/silkmc/silk/core/math/vector/VectorOperations.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/math/vector/VectorOperations.kt
@@ -2,7 +2,6 @@ package net.silkmc.silk.core.math.vector
 
 import net.minecraft.core.Vec3i
 import net.minecraft.world.phys.Vec3
-import org.joml.Vector3f
 
 operator fun Vec3.not(): Vec3 =
     reverse()
@@ -34,50 +33,14 @@ operator fun Vec3.times(vec: Vec3): Vec3 =
 operator fun Vec3.div(vec: Vec3): Vec3 =
     Vec3(x / vec.x, y / vec.y, z / vec.z)
 
+@Suppress("DeprecatedCallableAddReplaceWith") // a "replace with" quick-fix could be confusing here
+@Deprecated(message = "This comparison is unintuitive and should be avoided")
 operator fun Vec3.compareTo(vec: Vec3) =
     lengthSqr().compareTo(vec.lengthSqr())
 
 operator fun Vec3.component1() = x
 operator fun Vec3.component2() = y
 operator fun Vec3.component3() = z
-
-
-operator fun Vector3f.not(): Vector3f =
-    times(-1f)
-
-operator fun Vector3f.plus(n: Number): Vector3f =
-    Vector3f(this).apply { n.toFloat().let { nFloat -> add(nFloat, nFloat, nFloat) } }
-
-operator fun Vector3f.minus(n: Number): Vector3f =
-    Vector3f(this).apply { n.toFloat().let { nFloat -> sub(Vector3f(nFloat, nFloat, nFloat)) } }
-
-operator fun Vector3f.times(n: Number): Vector3f =
-    n.toFloat().let { nFloat -> Vector3f(x() * nFloat, y() * nFloat, z() * nFloat) }
-
-operator fun Vector3f.div(n: Number): Vector3f =
-    times(1.0 / n.toDouble())
-
-operator fun Vector3f.compareTo(n: Number) =
-    Vec3(this).length().compareTo(n.toDouble())
-
-operator fun Vector3f.plus(vec: Vector3f): Vector3f =
-    Vector3f(this).apply { add(vec) }
-
-operator fun Vector3f.minus(vec: Vector3f): Vector3f =
-    Vector3f(this).apply { sub(vec) }
-
-operator fun Vector3f.times(vec: Vector3f): Vector3f =
-    Vector3f(x() * vec.x(), y() * vec.y(), z() * vec.z())
-
-operator fun Vector3f.div(vec: Vector3f): Vector3f =
-    Vector3f(x() / vec.x(), y() / vec.y(), z() / vec.z())
-
-operator fun Vector3f.compareTo(vec: Vector3f) =
-    Vec3(this).lengthSqr().compareTo(Vec3(vec).lengthSqr())
-
-operator fun Vector3f.component1() = x()
-operator fun Vector3f.component2() = y()
-operator fun Vector3f.component3() = z()
 
 
 operator fun Vec3i.not(): Vec3i =
@@ -110,8 +73,8 @@ operator fun Vec3i.times(vec: Vec3i): Vec3i =
 operator fun Vec3i.div(vec: Vec3i): Vec3i =
     Vec3i(x / vec.x, y / vec.y, z / vec.z)
 
-operator fun Vec3i.compareTo(vec: Vec3i) =
-    Vec3(x.toDouble(), y.toDouble(), z.toDouble()).lengthSqr().compareTo(Vec3(vec.x.toDouble(), vec.y.toDouble(), vec.z.toDouble()).lengthSqr())
+// there is an internal compareTo function
+// operator fun Vec3i.compareTo(vec: Vec3i) = ...
 
 operator fun Vec3i.component1() = x
 operator fun Vec3i.component2() = y
