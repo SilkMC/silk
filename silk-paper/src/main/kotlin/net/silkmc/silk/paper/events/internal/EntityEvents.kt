30cd9d4c4485d9b25e96d7540ffbc254e5722ce7
Miraculixx <miraculixxyt@gmail.com>
2024-09-01 20:35:14 +0200

    Adding paper implementation for new events

--- silk-paper/src/main/kotlin/net/silkmc/silk/paper/events/internal/EntityEvents.kt
+++ silk-paper/src/main/kotlin/net/silkmc/silk/paper/events/internal/EntityEvents.kt
@@ -6,14 +6,68 @@ import net.silkmc.silk.core.event.EventScopeProperty
 import net.silkmc.silk.paper.conversions.mcEntity
 import net.silkmc.silk.paper.events.listenSilk
 import org.bukkit.craftbukkit.damage.CraftDamageSource
+import org.bukkit.craftbukkit.entity.CraftItem
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.entity.EntityDropItemEvent
 
 fun EntityEvents.setupPaper() {
     listenSilk<EntityDamageEvent> {
         val mcEntity = it.entity.mcEntity
-        checkInvulnerability.invoke(EntityEvents.EntityCheckInvulnerabilityEvent(mcEntity, (it.damageSource as CraftDamageSource).handle, EventScopeProperty(it.isCancelled)))
+
+        // Check invulnerability event - start
+        val checkInvulnerabilityEvent = EntityEvents.EntityCheckInvulnerabilityEvent(
+            mcEntity,
+            (it.damageSource as CraftDamageSource).handle,
+            EventScopeProperty(false)
+        )
+
+        checkInvulnerability.invoke(checkInvulnerabilityEvent)
+
+        if (checkInvulnerabilityEvent.isInvulnerable.get()) {
+            it.isCancelled = true
+            return@listenSilk
+        }
+        // Check invulnerability event - end
+
+        // Damage event - start
         if (mcEntity is LivingEntity) {
             damageLivingEntity.invoke(EntityEvents.EntityDamageEvent(mcEntity, it.finalDamage.toFloat(), (it.damageSource as CraftDamageSource).handle))
+        }
+        // Damage event - end
+    }
+
+    listenSilk<EntityDropItemEvent> {
+        val dropEvent = EntityEvents.EntityDropItemEvent(
+            it.entity.mcEntity,
+            (it.itemDrop as CraftItem).handle,
+            EventScopeProperty(it.isCancelled)
+        )
+
+        dropItem.invoke(dropEvent)
+
+        if (dropEvent.isCancelled.get()) {
+            it.isCancelled = true
+        }
+    }
+
+    listenSilk<EntityDeathEvent> {
+        val deathEvent = EntityEvents.EntityDeathEvent(
+            it.entity.mcEntity as LivingEntity,
+            (it.damageSource as CraftDamageSource).handle,
+            EventScopeProperty(it.drops.isNotEmpty()),
+            EventScopeProperty(it.isCancelled)
+        )
+
+        death.invoke(deathEvent)
+
+        if (deathEvent.isCancelled.get()) {
+            it.isCancelled = true
+        }
+
+        if (!deathEvent.isDroppingLoot.get()) {
+            it.drops.clear()
+            it.droppedExp = 0
         }
     }
 }
--- silk-paper/src/main/kotlin/net/silkmc/silk/paper/events/internal/PlayerEvents.kt
+++ silk-paper/src/main/kotlin/net/silkmc/silk/paper/events/internal/PlayerEvents.kt
@@ -1,9 +1,13 @@
 package net.silkmc.silk.paper.events.internal
 
 import io.papermc.paper.adventure.AdventureComponent
+import net.silkmc.silk.core.event.EventScopeProperty
 import net.silkmc.silk.core.event.PlayerEvents
+import net.silkmc.silk.core.text.LiteralTextBuilder
+import net.silkmc.silk.paper.conversions.adventureComponent
 import net.silkmc.silk.paper.conversions.mcPlayer
 import net.silkmc.silk.paper.events.listenSilk
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerLoginEvent
 import org.bukkit.event.player.PlayerQuitEvent
@@ -12,10 +16,18 @@ fun PlayerEvents.setupPaper() {
     listenSilk<PlayerLoginEvent> {
         preLogin.invoke(PlayerEvents.PlayerEvent(it.player.mcPlayer))
     }
+
     listenSilk<PlayerJoinEvent> {
         postLogin.invoke(PlayerEvents.PlayerEvent(it.player.mcPlayer))
     }
+
     listenSilk<PlayerQuitEvent> {
         preQuit.invoke(PlayerEvents.PlayerQuitEvent(it.player.mcPlayer, AdventureComponent(it.quitMessage())))
+    }
+
+    listenSilk<PlayerDeathEvent> {
+        val event = PlayerEvents.PlayerDeathMessageEvent(it.entity.mcPlayer, EventScopeProperty(AdventureComponent(it.deathMessage())))
+        deathMessage.invoke(event)
+        it.deathMessage(event.deathMessage.get().adventureComponent)
     }
 }
