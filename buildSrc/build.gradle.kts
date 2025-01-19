c76b0bd6b74baee08241ca46b7548d4a431d3556
Miraculixx <miraculixxyt@gmail.com>
2025-01-19 17:39:50 +0100

    Update loom to 1.9

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     compileOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", embeddedKotlinVersion))
     runtimeOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", kotlinVersion))
 
-    implementation(pluginDep("fabric-loom", "1.8-SNAPSHOT"))
+    implementation(pluginDep("fabric-loom", "1.9-SNAPSHOT"))
     implementation(pluginDep("com.modrinth.minotaur", "2.8.7"))
 
     implementation(pluginDep("io.papermc.paperweight.userdev", "1.7.2"))
