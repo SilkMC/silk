51538bf48cace8a9c07fe530cf3afb2b98529367
solonovamax <solonovamax@12oclockpoint.com>
2025-01-27 12:20:34 -0500

    Update to 1.21.4
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- silk-core/src/main/kotlin/net/silkmc/silk/core/recipe/Ingredient.kt
+++ silk-core/src/main/kotlin/net/silkmc/silk/core/recipe/Ingredient.kt
@@ -2,9 +2,9 @@
 
 package net.silkmc.silk.core.recipe
 
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.level.ItemLike
 
@@ -12,10 +12,10 @@ fun ingredientOf(vararg items: ItemLike): Ingredient {
     return Ingredient.of(*items)
 }
 
-fun ingredientOf(vararg items: ItemStack): Ingredient {
-    return Ingredient.of(*items)
+fun ingredientOf(items: List<ItemLike>): Ingredient {
+    return Ingredient.of(items.stream())
 }
 
 fun ingredientOf(items: TagKey<Item>): Ingredient {
-    return Ingredient.of(items)
+    return Ingredient.of(BuiltInRegistries.ITEM.getOrThrow(items))
 }
