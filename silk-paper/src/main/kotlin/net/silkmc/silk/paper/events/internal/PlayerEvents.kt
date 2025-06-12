cf360b896a66b60b0d0f89cb1bcc54f273fe6424
Paul K <pa179kin@gmail.com>
2025-06-13 01:46:39 +0200

    add paper implementation for chat event

--- silk-paper/src/main/kotlin/net/silkmc/silk/paper/events/internal/PlayerEvents.kt
+++ silk-paper/src/main/kotlin/net/silkmc/silk/paper/events/internal/PlayerEvents.kt
@@ -2,9 +2,14 @@ package net.silkmc.silk.paper.events.internal
 
 import io.papermc.paper.adventure.AdventureComponent
 import io.papermc.paper.adventure.PaperAdventure
+import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import net.minecraft.network.chat.ChatType
+import net.minecraft.network.chat.PlayerChatMessage
 import net.silkmc.silk.core.event.EventScopeProperty
 import net.silkmc.silk.core.event.PlayerEvents
 import net.silkmc.silk.paper.conversions.mcDamageSource
+import net.silkmc.silk.paper.conversions.mcEntity
 import net.silkmc.silk.paper.conversions.mcPlayer
 import net.silkmc.silk.paper.events.listenSilk
 import org.bukkit.event.entity.PlayerDeathEvent
@@ -38,5 +43,22 @@ fun PlayerEvents.setupPaper() {
         )
         onDeath.invoke(event)
         it.deathMessage(PaperAdventure.asAdventure(event.deathMessage.get()))
+    }
+
+    listenSilk<AsyncChatEvent> {
+        val message: PlayerChatMessage = PlayerChatMessage.unsigned(
+            it.player.uniqueId,
+            PlainTextComponentSerializer.plainText().serialize(it.message())
+        )
+        val bound: ChatType.Bound = ChatType.bind(ChatType.CHAT, it.player.mcEntity)
+        val event = PlayerEvents.PlayerChatEvent(
+            it.player.mcPlayer,
+            message,
+            bound
+        )
+
+        PlayerEvents.onChat.invoke(event)
+        it.isCancelled = event.isCancelled.get()
+        it.message(PaperAdventure.asAdventure(event.message.decoratedContent()))
     }
 }
