5a25f7022fd7954e4e029175b409aac62ab85629
Paul K <git@pauli.fyi>
2026-06-17 16:51:32 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 allprojects {
     group = "net.silkmc"
-    version = "1.11.9"
+    version = "1.11.10"
     if (this.name.startsWith("silk")) {
         description = "Silk is a Minecraft API for Kotlin"
     }
